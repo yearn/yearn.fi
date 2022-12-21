b1760d656724bd0b084b4a104751a77b5aff37bc
Karelian Pie <devkarelianpie@gmail.com>
2022-12-21 11:48:55 +0200

    refactor: Reduce the min width

--- pages/ycrv/about.tsx
+++ pages/ycrv/about.tsx
@@ -125,7 +125,7 @@ function	About(): ReactElement {
 						{'Slippage tolerance'}
 					</label>
 					<div className={'flex flex-row space-x-2'}>
-						<div className={'flex h-10 w-40 min-w-[160px] items-center border-2 border-neutral-700 bg-neutral-0 py-4 px-0'}>
+						<div className={'flex h-10 w-40 min-w-[72px] items-center border-2 border-neutral-700 bg-neutral-0 py-4 px-0'}>
 							<input
 								id={'slippageTolerance'}
 								type={'number'}
