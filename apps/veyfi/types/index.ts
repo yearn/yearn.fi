f6c0bc280ac048aaf3b7de576356b727876e7f24
xgambitox <18649057+xgambitox@users.noreply.github.com>
2022-12-28 15:59:32 -0600

    feat: add time utils

---
+++ apps/veyfi/types/index.ts
@@ -0,0 +1,12 @@
+export type TAmount = string;
+
+export type TFraction = string;
+
+export type TFormattedAmount = string;
+
+export type TRaw = string;
+export type TUnit = string;
+
+export type TSeconds = number;
+export type TMilliseconds = number;
+export type TWeeks = number;
