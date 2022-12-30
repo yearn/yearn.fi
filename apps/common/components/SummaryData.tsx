4e0b52c1eed98ff19744ba06d8ffb7a750a09aa5
xgambitox <18649057+xgambitox@users.noreply.github.com>
2022-12-30 12:45:11 -0600

    feat: add summary data common component

---
+++ apps/common/components/SummaryData.tsx
@@ -0,0 +1,30 @@
+
+import type {ReactElement} from 'react';
+
+type TItem = {
+    label: string,
+    content: string
+}
+
+type TTabsProps = {
+	items: TItem[],
+}
+
+function SummaryData({items}: TTabsProps): ReactElement {
+	return (
+		<div className={'align-center flex w-full flex-row flex-wrap justify-center gap-14'}>
+			{items.map(((item): ReactElement => (
+				<div key={item.label} className={'flex flex-col items-center justify-center space-y-2'}>
+					<b className={'font-number text-3xl'}>
+						{item.content}
+					</b>
+					<p className={'text-center text-xs text-neutral-600'}>
+						{item.label}
+					</p>
+				</div>
+			)))}
+		</div>
+	);
+}
+
+export {SummaryData};
