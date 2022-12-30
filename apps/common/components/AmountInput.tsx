4528497e56a1af7ecb992ae65ffb0fa8d0a1c1bc
xgambitox <18649057+xgambitox@users.noreply.github.com>
2022-12-30 13:17:59 -0600

    feat: add amount input common component

---
+++ apps/common/components/AmountInput.tsx
@@ -0,0 +1,63 @@
+import type {ReactElement} from 'react';
+
+type TAmountInputProps = {
+	amount?: string;
+	onAmountChange?: (amount: string) => void;
+	maxAmount?: string;
+	maxLabel?: string;
+	label?: string;
+	placeholder?: string;
+	legend?: string;
+	error?: string;
+	disabled?: boolean;
+	loading?: boolean;
+}
+
+function AmountInput({
+	amount,
+	onAmountChange,
+	maxAmount,
+	maxLabel = 'Max',
+	label,
+	placeholder,
+	legend,
+	error,
+	disabled,
+	loading
+}: TAmountInputProps): ReactElement {
+	return (
+		<div className={'w-full'}>
+			{label && (
+				<p
+					className={'mb-1 w-full overflow-hidden text-ellipsis whitespace-nowrap text-base text-neutral-600'}>
+					{label}
+				</p>
+			)}
+			<div className={'relative flex w-full items-center justify-center'}>
+				<input
+					className={`h-10 w-full p-2 font-mono text-base font-normal outline-none ${maxAmount && !disabled ? 'pr-12' : null} ${error ? 'border border-solid border-[#EA5204] focus:border-[#EA5204]' : 'border-0 border-none'} ${disabled ? 'bg-neutral-300 text-[#5B5B5B]' : null}`}
+					type={'number'}
+					aria-label={label}
+					value={amount}
+					onChange={onAmountChange ? (e): void => onAmountChange(e.target.value) : undefined}
+					placeholder={loading ? '' : placeholder ?? '0'}
+					disabled={disabled}
+				/>
+				{maxAmount && !disabled && (
+					<button
+						onClick={onAmountChange ? (): void => onAmountChange(maxAmount) : undefined}
+						className={'absolute right-2 ml-2 h-6 cursor-pointer border-none bg-neutral-900 px-2 py-1 text-xs text-neutral-0 transition-colors hover:bg-neutral-700'}>
+						{maxLabel}
+					</button>
+				)}
+			</div>
+			{(error || legend) && (
+				<legend className={`mt-1 pl-2 text-xs md:mr-0 ${error ? 'text-[#EA5204]' : 'text-neutral-600'}`} >
+					{error ?? legend}
+				</legend>
+			)}
+		</div>
+	);
+}
+
+export {AmountInput};
