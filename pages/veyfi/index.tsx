6c90acce61b66b997f8aa867397ff96e35946dfa
xgambitox <18649057+xgambitox@users.noreply.github.com>
2023-01-03 13:08:13 -0600

    feat: add main veyfi app page

--- pages/veyfi/index.tsx
+++ pages/veyfi/index.tsx
@@ -1,19 +1,47 @@
 import React from 'react';
+import {ClaimTab} from '@veYFI/components/ClaimTab';
+import {LockTab} from '@veYFI/components/LockTab';
+import {ManageLockTab} from '@veYFI/components/ManageLockTab';
+import {useVotingEscrow} from '@veYFI/contexts/useVotingEscrow';
+import {formatAmount, toUnit} from '@veYFI/utils/format';
+import Wrapper from '@veYFI/Wrapper';
+import {SummaryData} from '@common/components/SummaryData';
+import {Tabs} from '@common/components/Tabs';
 
 import type {ReactElement} from 'react';
 
-function	Index(): ReactElement {
+function Index(): ReactElement {
+	const {votingEscrow, positions} = useVotingEscrow();
+
 	return (
-		<div className={'mx-auto mt-20 mb-44 flex w-full max-w-6xl flex-col items-center justify-center'}>
-			<div className={'relative h-12 w-[300px] md:h-[104px] md:w-[600px]'}>
-				{'Soon'}
+		<>
+			<h1 className={'w-full text-center text-8xl font-bold'}>
+				{'veYFI'}
+			</h1>
+			
+			<div className={'my-14 w-full'}>
+				<SummaryData
+					items={[
+						{label: 'Total Locked YFI', content: formatAmount(toUnit(votingEscrow?.supply, 18), 4)},
+						{label: 'Your Locked YFI', content: formatAmount(toUnit(positions?.deposit?.underlyingBalance, 18), 4)},
+						{label: 'Expiration for the lock', content: positions?.unlockTime ? new Date(positions.unlockTime).toLocaleDateString('en-CA') : '-'}
+					]} />
 			</div>
-		</div>
+
+			<Tabs
+				className={'min-h-[356px]'}
+				items={[
+					{id: 'lock', label: 'Lock YFI', content: <LockTab />}, 
+					{id: 'manage', label: 'Manage lock', content: <ManageLockTab />}, 
+					{id: 'claim', label: 'Claim', content: <ClaimTab />}
+				]} />
+			
+		</>
 	);
 }
 
 Index.getLayout = function getLayout(page: ReactElement): ReactElement {
-	return page;
+	return <Wrapper>{page}</Wrapper>;
 };
 
 export default Index;
