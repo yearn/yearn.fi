2b7192cb62d6a4048e01dd6c0361945fb8067e8b
xgambitox <18649057+xgambitox@users.noreply.github.com>
2023-01-04 15:51:39 -0600

    refactor: remove app format amount util

--- apps/veyfi/utils/format.ts
+++ apps/veyfi/utils/format.ts
@@ -1,17 +1,7 @@
 import {ethers} from 'ethers';
 
-import type {TAmount, TFormattedAmount, TRaw, TUnit} from '@veYFI/types';
+import type {TRaw, TUnit} from '@veYFI/types';
 
 export const toRaw = (amount: TUnit, decimals: number): TRaw => {
 	return ethers.utils.parseUnits(amount || '0', decimals).toString();
 };
-
-export const formatAmount = (amount: TAmount, decimals: number): TFormattedAmount =>
-	asAmount(Number(amount), decimals, decimals);
-
-function asAmount(amount: number, minimumFractionDigits = 2, maximumFractionDigits = 2): string {
-	if (maximumFractionDigits < minimumFractionDigits) {
-		maximumFractionDigits = minimumFractionDigits;
-	}
-	return new Intl.NumberFormat(['en-US'], {minimumFractionDigits, maximumFractionDigits}).format(amount);
-}
