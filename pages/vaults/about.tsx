bdb7193d1e68796d35cd31cb6bbf7b54b0137790
Karelian Pie <devkarelianpie@gmail.com>
2023-01-05 18:21:25 +0200

    refactor: Group the text block rather than individual paragraphs

--- pages/vaults/about.tsx
+++ pages/vaults/about.tsx
@@ -14,15 +14,17 @@ function	About(): ReactElement {
 					<h2 className={'text-3xl font-bold'}>{'Wtf is a Vault?'}</h2>
 				</div>
 				<div aria-label={'Wtf is a Vault? details'}>
-					<p className={'pb-4 text-neutral-600'}>
-						<Balancer>{'In ‘traditional finance’ (boo, hiss) you can earn yield on your savings by depositing them in a bank - who use the capital for loans and other productive money growing means.'}</Balancer>
-					</p>
-					<p className={'pb-4 text-neutral-600'}>
-						<Balancer>{'Yearn Vaults are like crypto savings accounts floating in cyberspace. You deposit your assets, and Yearn puts them to work within the DeFi ecosystem, returning the earned yield back to you.'}</Balancer>
-					</p>
-					<p className={'text-neutral-600'}>
-						<Balancer>{'However, unlike a bank account - none of this takes place behind closed doors (no offence to doors). Decentralised Finance uses public blockchains, meaning you are in control of your assets and can see where they are at all times. Nothing is hidden and everything is auditable by anyone, at any time.'}</Balancer>
-					</p>
+					<Balancer>
+						<p className={'pb-4 text-neutral-600'}>
+							{'In ‘traditional finance’ (boo, hiss) you can earn yield on your savings by depositing them in a bank - who use the capital for loans and other productive money growing means.'}
+						</p>
+						<p className={'pb-4 text-neutral-600'}>
+							{'Yearn Vaults are like crypto savings accounts floating in cyberspace. You deposit your assets, and Yearn puts them to work within the DeFi ecosystem, returning the earned yield back to you.'}
+						</p>
+						<p className={'text-neutral-600'}>
+							{'However, unlike a bank account - none of this takes place behind closed doors (no offence to doors). Decentralised Finance uses public blockchains, meaning you are in control of your assets and can see where they are at all times. Nothing is hidden and everything is auditable by anyone, at any time.'}
+						</p>
+					</Balancer>
 				</div>
 			</div> 
 
@@ -31,14 +33,15 @@ function	About(): ReactElement {
 					<h2 className={'text-3xl font-bold'}>{'Risk Score'}</h2>
 				</div>
 				<div aria-label={'Risk Score details'}>
-					<p className={'pb-4 text-neutral-600'}>
-						<Balancer>{'In order to give users the best risk-adjusted yields in DeFi, Yearn uses a comprehensive risk assessment framework for each strategy within a Vault. This framework combines to give each Vault a holistic Risk Score.'}</Balancer>
-					</p>
-					<p className={'pb-4 text-neutral-600'}>
-						<Balancer>{'Strategies are assessed against eight different factors; Audit, Code Review, Complexity, Longevity, Protocol Safety, Team Knowledge, Testing Score, TVL Impact. Since Vaults use multiple strategies, riskier strategies can be paired with more conservative ones to ensure the Vault has a robust and balanced Risk Score.'}</Balancer>
-					</p>
-					<p className={'text-neutral-600'}>
-						<Balancer>{'For a full breakdown read more about our '}
+					<Balancer>
+						<p className={'pb-4 text-neutral-600'}>
+							{'In order to give users the best risk-adjusted yields in DeFi, Yearn uses a comprehensive risk assessment framework for each strategy within a Vault. This framework combines to give each Vault a holistic Risk Score.'}
+						</p>
+						<p className={'pb-4 text-neutral-600'}>
+							{'Strategies are assessed against eight different factors; Audit, Code Review, Complexity, Longevity, Protocol Safety, Team Knowledge, Testing Score, TVL Impact. Since Vaults use multiple strategies, riskier strategies can be paired with more conservative ones to ensure the Vault has a robust and balanced Risk Score.'}
+						</p>
+						<p className={'text-neutral-600'}>
+							{'For a full breakdown read more about our '}
 							<a
 								href={'https://docs.yearn.finance/resources/risks/risk-score'}
 								target={'_blank'}
@@ -47,8 +50,8 @@ function	About(): ReactElement {
 								{'Risk Scores'}
 							</a>
 							{'.'}
-						</Balancer>
-					</p>
+						</p>
+					</Balancer>
 				</div>
 			</div>
 
@@ -81,14 +84,15 @@ function	About(): ReactElement {
 					<h2 className={'text-3xl font-bold'}>{'Yearn? DeFi? I think I’m lost…'}</h2>
 				</div>
 				<div aria-label={'Yearn? DeFi? I think I’m lost… details'}>
-					<p className={'pb-4 text-neutral-600'}>
-						<Balancer>{'Searching for ‘words that rhyme with turn’ and accidentally ended up here? Welcome! You’re at the frontier of Decentralised Finance - a new type of financial system built on blockchains and designed to give users better access, transparency and control of their assets.'}</Balancer>
-					</p>
-					<p className={'pb-4 text-neutral-600'}>
-						<Balancer>{'DeFi offers many opportunities to put your digital assets to work, and earn yield in return - and Yearn was designed to automate this process for you. Less sharp suits and slicked back hair, more cyberspace yield ninjas wielding razor sharp battle tested code katanas.'}</Balancer>
-					</p>
-					<p className={'text-neutral-600'}>
-						<Balancer>{'We can’t offer you a phone number with ambient jazz hold music to listen to - but please feel free to hop into our '}
+					<Balancer>
+						<p className={'pb-4 text-neutral-600'}>
+							{'Searching for ‘words that rhyme with turn’ and accidentally ended up here? Welcome! You’re at the frontier of Decentralised Finance - a new type of financial system built on blockchains and designed to give users better access, transparency and control of their assets.'}
+						</p>
+						<p className={'pb-4 text-neutral-600'}>
+							{'DeFi offers many opportunities to put your digital assets to work, and earn yield in return - and Yearn was designed to automate this process for you. Less sharp suits and slicked back hair, more cyberspace yield ninjas wielding razor sharp battle tested code katanas.'}
+						</p>
+						<p className={'text-neutral-600'}>
+							{'We can’t offer you a phone number with ambient jazz hold music to listen to - but please feel free to hop into our '}
 							<a
 								href={'https://discord.com/invite/6PNv2nF'}
 								target={'_blank'}
@@ -97,8 +101,8 @@ function	About(): ReactElement {
 								{'discord'}
 							</a>
 							{' if you have any questions, we’d love to chat.'}
-						</Balancer>
-					</p>
+						</p>
+					</Balancer>
 				</div>
 			</div>
 
