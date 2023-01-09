b46608bd67e96b7e256ebfc71fefb184bf4e5209
xgambitox <18649057+xgambitox@users.noreply.github.com>
2023-01-09 11:03:05 -0600

    refactor: destructure component

--- apps/common/components/SummaryData.tsx
+++ apps/common/components/SummaryData.tsx
@@ -13,13 +13,13 @@ type TTabsProps = {
 function SummaryData({items}: TTabsProps): ReactElement {
 	return (
 		<div className={'align-center flex w-full flex-row flex-wrap justify-center gap-14'}>
-			{items.map(((item): ReactElement => (
-				<div key={item.label} className={'flex flex-col items-center justify-center space-y-2'}>
+			{items.map((({label, content}): ReactElement => (
+				<div key={label} className={'flex flex-col items-center justify-center space-y-2'}>
 					<b className={'font-number text-3xl'} suppressHydrationWarning>
-						{item.content}
+						{content}
 					</b>
 					<p className={'text-center text-xs text-neutral-600'}>
-						{item.label}
+						{label}
 					</p>
 				</div>
 			)))}
