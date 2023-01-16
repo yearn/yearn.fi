754582671f4a9380544e49585937bac3e424e6d9
Karelian Pie <devkarelianpie@gmail.com>
2023-01-16 15:43:04 +0200

    feat: Implement gauges sort and search

--- apps/ycrv/components/list/GaugeList.tsx
+++ apps/ycrv/components/list/GaugeList.tsx
@@ -1,13 +1,16 @@
-import React, {useMemo, useState} from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 import VaultListOptions from '@vaults/components/list/VaultListOptions';
-import {useAppSettings} from '@vaults/contexts/useAppSettings';
+import {useSessionStorage} from '@yearn-finance/web-lib/hooks/useSessionStorage';
+import performBatchedUpdates from '@yearn-finance/web-lib/utils/performBatchedUpdates';
 import ListHead from '@common/components/ListHead';
 import ListHero from '@common/components/ListHero';
+import {useSortGauges} from '@yCRV/hooks/useSortGauges';
 
 import {GaugeListEmpty} from './GaugeListEmpty';
 import {GaugeListRow} from './GaugeListRow';
 
 import type {ReactElement, ReactNode} from 'react';
+import type {TPossibleGaugesSortBy, TPossibleGaugesSortDirection} from '@yCRV/hooks/useSortGauges';
 import type {TCurveGauges} from '@common/types/curves';
 
 type TProps = {
@@ -16,32 +19,52 @@ type TProps = {
 }
 
 function	GaugeList({gauges, isLoadingGauges}: TProps): ReactElement {
-	const	{searchValue, set_searchValue} = useAppSettings();
 	const	[category, set_category] = useState('Standard');
+	const 	[searchValue, set_searchValue] = useSessionStorage('yCRVVoteSearchValue', '');
+	const	[sortBy, set_sortBy] = useState<TPossibleGaugesSortBy>('name');
+	const	[sortDirection, set_sortDirection] = useState<TPossibleGaugesSortDirection>('');
 
+	const	searchedGauges = useMemo((): TCurveGauges[] => {
+		if (searchValue === '') {
+			return gauges;
+		}
+
+		return gauges.filter(({name}): boolean => {
+			return name.toLowerCase().includes(searchValue.toLowerCase());
+		});
+	}, [searchValue, gauges]);
+
+	const	sortedGaugesToDisplay = useSortGauges([...searchedGauges], sortBy, sortDirection);
+
+	const	onSort = useCallback((newSortBy: string, newSortDirection: string): void => {
+		performBatchedUpdates((): void => {
+			set_sortBy(newSortBy as TPossibleGaugesSortBy);
+			set_sortDirection(newSortDirection as TPossibleGaugesSortDirection);
+		});
+	}, []);
 
 	/* 🔵 - Yearn Finance **************************************************************************
 	**	The GaugeList component is memoized to prevent it from being re-created on every render.
 	**	It contains either the list of gauges, is some are available, or a message to the user.
 	**********************************************************************************************/
 	const	GaugeList = useMemo((): ReactNode => {
-		if (isLoadingGauges || gauges.length === 0) {
+		if (isLoadingGauges || sortedGaugesToDisplay.length === 0) {
 			return (
 				<GaugeListEmpty
 					isLoading={isLoadingGauges}
-					gauges={gauges}
+					gauges={sortedGaugesToDisplay}
 					currentCategory={category} />
 			);	
 		}
 		return (
-			gauges.map((gauge): ReactNode => {
+			sortedGaugesToDisplay.map((gauge): ReactNode => {
 				if (!gauge) {
 					return (null);
 				}
 				return <GaugeListRow key={gauge.name} gauge={gauge} />;
 			})
 		);
-	}, [category, gauges, isLoadingGauges]);
+	}, [category, isLoadingGauges, sortedGaugesToDisplay]);
 
 	return (
 		<div className={'relative col-span-12 flex w-full flex-col bg-neutral-100'}>
@@ -65,9 +88,9 @@ function	GaugeList({gauges, isLoadingGauges}: TProps): ReactElement {
 				set_searchValue={set_searchValue} />
 
 			<ListHead
-				sortBy={''}
-				sortDirection={''}
-				onSort={(): void => alert('Not implemented!')}
+				sortBy={sortBy}
+				sortDirection={sortDirection}
+				onSort={onSort}
 				items={[
 					{label: 'Gauges', value: 'gauges', sortable: true},
 					{label: 'Multi Select', value: 'multi-select', className: 'col-span-1'},
