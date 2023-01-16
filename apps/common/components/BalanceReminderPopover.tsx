a7acf8f1e05a4917dd040b14431b09e03bcb0415
Majorfi <major-tom.eth@pm.me>
2023-01-16 20:46:01 +0100

    feat: new login popover

--- apps/common/components/BalanceReminderPopover.tsx
+++ apps/common/components/BalanceReminderPopover.tsx
@@ -124,49 +124,50 @@ export default function BalanceReminderPopover(): ReactElement {
 						<IconWallet className={'yearn--header-nav-item mt-0.5 h-4 w-4'} />
 					</Popover.Button>
 					<Transition
-						as={Fragment}
+						as={'span'}
+						appear
+						unmount={false}
 						enter={'transition ease-out duration-200'}
 						enterFrom={'opacity-0 translate-y-1'}
 						enterTo={'opacity-100 translate-y-0'}
-						leave={'transition ease-in duration-150'}
+						leave={'transition ease-in duration-200'}
 						leaveFrom={'opacity-100 translate-y-0'}
-						leaveTo={'opacity-0 translate-y-1'}>
+						leaveTo={'opacity-0 translate-y-1'}
+						className={'z-40'}>
 						<Popover.Panel className={'absolute right-0 top-6 z-[1000] mt-3 w-screen max-w-xs md:top-4 md:-right-4 '}>
-							<div className={'overflow-hidden'}>
-								<div className={'relative bg-neutral-100 p-0'}>
-									<div className={'flex items-center justify-center border-b border-neutral-300 py-4 text-center'}>
-										<b>
-											{isActive && address && ens ? (
-												ens
-											) : isActive && address ? (
-												truncateHex(address, 5)
-											) : 'Connect wallet'}
-										</b>
-									</div>
-									<div className={'absolute top-4 right-4'}>
-										<button
-											onClick={onDesactivate}
-											className={'flex h-6 w-6 items-center justify-center rounded-full bg-neutral-200/50'}>
-											<IconCross className={'h-4 w-4 text-neutral-600'} />
-										</button>
-									</div>
-
-									{
-										(nonNullBalancesForVault.length === 0 && isLoading) ? (
-											<div className={'py-4 text-center text-sm text-neutral-400'}>
-												{'Retrieving your yvTokens ...'}
-											</div>
-										) : (nonNullBalancesForVault.length === 0) ? (
-											<div className={'py-4 text-center text-sm text-neutral-400'}>
-												{'No position in Yearn found.'}
-											</div>
-										) : nonNullBalancesForVault.map((element): ReactElement => (
-											<TokenItem
-												key={element.address}
-												element={element} />
-										))
-									}
+							<div className={'yearn--shadow z-50 overflow-hidden'}>
+								<div className={'flex items-center justify-center border-b border-neutral-300 py-4 text-center'}>
+									<b>
+										{isActive && address && ens ? (
+											ens
+										) : isActive && address ? (
+											truncateHex(address, 5)
+										) : 'Connect wallet'}
+									</b>
 								</div>
+								<div className={'absolute top-4 right-4'}>
+									<button
+										onClick={onDesactivate}
+										className={'flex h-6 w-6 items-center justify-center rounded-full bg-neutral-200/50'}>
+										<IconCross className={'h-4 w-4 text-neutral-600'} />
+									</button>
+								</div>
+
+								{
+									(nonNullBalancesForVault.length === 0 && isLoading) ? (
+										<div className={'py-4 text-center text-sm text-neutral-400'}>
+											{'Retrieving your yvTokens ...'}
+										</div>
+									) : (nonNullBalancesForVault.length === 0) ? (
+										<div className={'py-4 text-center text-sm text-neutral-400'}>
+											{'No position in Yearn found.'}
+										</div>
+									) : nonNullBalancesForVault.map((element): ReactElement => (
+										<TokenItem
+											key={element.address}
+											element={element} />
+									))
+								}
 							</div>
 						</Popover.Panel>
 					</Transition>
