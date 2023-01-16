181eab8a6e0f8006619df08f36efb335e5d75f95
xgambitox <18649057+xgambitox@users.noreply.github.com>
2023-01-16 10:20:02 -0600

    feat: add delegate vote action

---
+++ apps/veyfi/utils/actions/votingEscrow.ts
@@ -0,0 +1,115 @@
+import {ethers} from 'ethers';
+import {approveERC20} from '@common/utils/actions';
+
+import SNAPSHOT_DELEGATE_REGISTRY_ABI from '../abi/SnapshotDelegateRegistry.abi';
+import VEYFI_ABI from '../abi/veYFI.abi';
+import {SNAPSHOT_DELEGATE_REGISTRY_ADDRESS, YEARN_SNAPSHOT_SPACE} from '../constants';
+
+import type {BigNumber} from 'ethers';
+import type {Connector} from 'wagmi';
+import type {TAddress} from '@yearn-finance/web-lib/types';
+import type {TSeconds} from '@yearn-finance/web-lib/utils/time';
+
+const handleTx = async (txPromise: Promise<ethers.providers.TransactionResponse>): Promise<boolean> => {
+	try {
+		const tx = await txPromise;
+		const receipt = await tx.wait();
+		if (receipt.status === 0) {
+			console.error('Fail to perform transaction');
+			return false;
+		}
+		return true;
+	} catch (error) {
+		console.error(error);
+		return false;
+	}
+};
+
+export async function approveLock(
+	provider: Connector,
+	_accountAddress: TAddress,
+	tokenAddress: TAddress,
+	votingEscrowAddress: TAddress,
+	amount?: bigint
+): Promise<boolean> {
+	if (!amount) {
+		return false;
+	}
+
+	const result = await approveERC20({
+		connector: provider,
+		contractAddress: tokenAddress,
+		spenderAddress: votingEscrowAddress,
+		amount
+	});
+	
+	return result.isSuccessful;
+}
+
+export async function lock(
+	provider: ethers.providers.Web3Provider,
+	accountAddress: TAddress,
+	votingEscrowAddress: TAddress,
+	amount: BigNumber,
+	time: TSeconds
+): Promise<boolean> {
+	const signer = provider.getSigner(accountAddress);
+	const votingEscrowContract = new ethers.Contract(votingEscrowAddress, VEYFI_ABI, signer);
+	return handleTx(votingEscrowContract.modify_lock(amount, time, accountAddress));
+}
+
+export async function increaseLockAmount(
+	provider: ethers.providers.Web3Provider,
+	accountAddress: TAddress,
+	votingEscrowAddress: TAddress,
+	amount: BigNumber
+): Promise<boolean> {
+	const signer = provider.getSigner(accountAddress);
+	const votingEscrowContract = new ethers.Contract(votingEscrowAddress, VEYFI_ABI, signer);
+	return handleTx(votingEscrowContract.modify_lock(amount, '0', accountAddress));
+}
+
+export async function extendLockTime(
+	provider: ethers.providers.Web3Provider,
+	accountAddress: TAddress,
+	votingEscrowAddress: TAddress,
+	time: TSeconds
+): Promise<boolean> {
+	const signer = provider.getSigner(accountAddress);
+	const votingEscrowContract = new ethers.Contract(votingEscrowAddress, VEYFI_ABI, signer);
+	return handleTx(votingEscrowContract.modify_lock('0', time, accountAddress));
+}
+
+export async function withdrawUnlocked(
+	provider: ethers.providers.Web3Provider,
+	accountAddress: TAddress,
+	votingEscrowAddress: TAddress
+): Promise<boolean> {
+	const signer = provider.getSigner(accountAddress);
+	const votingEscrowContract = new ethers.Contract(votingEscrowAddress, VEYFI_ABI, signer);
+	const {penalty} = await votingEscrowContract.callStatic.withdraw();
+	if ((penalty as BigNumber).gt(0)) {
+		throw new Error('Tokens are not yet unlocked');
+	}
+	return handleTx(votingEscrowContract.withdraw());
+}
+
+export async function withdrawLocked(
+	provider: ethers.providers.Web3Provider,
+	accountAddress: TAddress,
+	votingEscrowAddress: TAddress
+): Promise<boolean> {
+	const signer = provider.getSigner(accountAddress);
+	const votingEscrowContract = new ethers.Contract(votingEscrowAddress, VEYFI_ABI, signer);
+	return handleTx(votingEscrowContract.withdraw());
+}
+
+export async function delegateVote(
+	provider: ethers.providers.Web3Provider,
+	accountAddress: TAddress,
+	delegateAddress: TAddress
+): Promise<boolean> {
+	const signer = provider.getSigner(accountAddress);
+	const delegateRegistryContract = new ethers.Contract(SNAPSHOT_DELEGATE_REGISTRY_ADDRESS, SNAPSHOT_DELEGATE_REGISTRY_ABI, signer);
+	return handleTx(delegateRegistryContract.setDelegate(ethers.utils.formatBytes32String(YEARN_SNAPSHOT_SPACE), delegateAddress));
+}
--- apps/veyfi/utils/constants.ts
+++ apps/veyfi/utils/constants.ts
@@ -4,3 +4,6 @@ import type {TWeeks} from '@yearn-finance/web-lib/utils/time';
 export const MAX_LOCK_TIME: TWeeks = 208;
 export const MIN_LOCK_TIME: TWeeks = 1;
 export const MIN_LOCK_AMOUNT: TWeeks = 1;
+
+export const YEARN_SNAPSHOT_SPACE = 'veyfi.eth';
+export const SNAPSHOT_DELEGATE_REGISTRY_ADDRESS = toAddress('0x469788fE6E9E9681C6ebF3bF78e7Fd26Fc015446');
