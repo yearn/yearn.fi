eccc2217518860933f36cb66ab092b7c712a952c
Karelian Pie <devkarelianpie@gmail.com>
2023-01-17 19:18:43 +0200

    feat: Vote components and withdraw tab

---
+++ apps/common/hooks/useTabs.tsx
@@ -0,0 +1,123 @@
+import {useMemo, useState} from 'react';
+import {AnimatePresence, motion} from 'framer-motion';
+import {Listbox, Transition} from '@headlessui/react';
+import IconChevron from '@common/icons/IconChevron';
+
+import type {Dispatch, ReactElement, SetStateAction} from 'react';
+
+const variants = {
+	initial: {y: 10, opacity: 0},
+	enter: {y: 0, opacity: 1},
+	exit: {y: -10, opacity: 0}
+};
+
+type TItem = {
+	id: string,
+	label: string,
+	content: ReactElement
+}
+
+type TTabsProps = {
+	items: TItem[],
+	className?: string,
+}
+
+type TUseTabs = {
+	component: ReactElement;
+	selectedTabId: string;
+	set_selectedTabId: Dispatch<SetStateAction<string>>;
+}
+
+function useTabs({items, className}: TTabsProps): TUseTabs {
+	if (items.length === 0) {
+		throw new Error('You need to pass at least one item for the tabs');
+	}
+
+	const [selectedTabId, set_selectedTabId] = useState(items[0].id);
+
+	const component = useMemo((): ReactElement => {
+		return (
+			<div className={`w-full bg-neutral-100 ${className}`}>
+				<nav className={'hidden h-14 w-full border-b-2 border-neutral-300 pl-[13px] pr-4 text-center md:flex'}>
+					{items.map(({id, label}): ReactElement => (
+						<div
+							key={`tab-label-${id}`}
+							className={`yearn--tab ${selectedTabId === id ? 'selected' : ''}`}
+							onClick={(): void => set_selectedTabId(id)}>
+							<p
+								title={label}
+								aria-selected={selectedTabId === id}
+								className={'hover-fix align-center flex grow flex-col justify-center'}>
+								{label}
+							</p>
+							{selectedTabId === id && <motion.div className={'relative -bottom-0.5 w-full border-b-[3px] border-neutral-700'} layoutId={'tab-label-underline'} />}
+							{selectedTabId !== id && <motion.div className={'relative -bottom-0.5 w-full border-b-[3px] border-transparent'} />}
+						</div>
+					))}
+				</nav>
+				<div className={'relative z-50 px-4 pt-4'}>
+					<Listbox
+						value={selectedTabId}
+						onChange={(value): void => set_selectedTabId(value)}>
+						{({open}): ReactElement => (
+							<>
+								<Listbox.Button
+									className={'flex h-10 w-full flex-row items-center border-0 border-b-2 border-neutral-900 bg-neutral-100 p-0 font-bold focus:border-neutral-900 md:hidden'}>
+									<div className={'relative flex flex-row items-center'}>
+										{items.find(({id}): boolean => id === selectedTabId)?.label || 'Menu'}
+									</div>
+									<div className={'absolute right-4'}>
+										<IconChevron
+											className={`h-6 w-6 transition-transform ${open ? '-rotate-180' : 'rotate-0'}`} />
+									</div>
+								</Listbox.Button>
+								<Transition
+									show={open}
+									enter={'transition duration-100 ease-out'}
+									enterFrom={'transform scale-95 opacity-0'}
+									enterTo={'transform scale-100 opacity-100'}
+									leave={'transition duration-75 ease-out'}
+									leaveFrom={'transform scale-100 opacity-100'}
+									leaveTo={'transform scale-95 opacity-0'}>
+									<Listbox.Options className={'yearn--listbox-menu'}>
+										{items.map(({id, label}): ReactElement => (
+											<Listbox.Option
+												className={'yearn--listbox-menu-item'}
+												key={id}
+												value={id}>
+												{label}
+											</Listbox.Option>
+										))}
+									</Listbox.Options>
+								</Transition>
+							</>
+						)}
+					</Listbox>
+				</div>
+				<AnimatePresence mode={'wait'}>
+					<motion.div
+						key={selectedTabId}
+						initial={'initial'}
+						animate={'enter'}
+						exit={'exit'}
+						variants={variants}
+						transition={{duration: 0.15}}
+					>
+						{items.map(({id, content}): ReactElement => (
+							<div
+								key={`tab-content-${id}`}
+								className={'w-full p-8'}
+								hidden={selectedTabId !== id}>
+								{content}
+							</div>
+						))}
+					</motion.div>
+				</AnimatePresence>
+			</div>
+		);
+	}, [className, items, selectedTabId]);
+
+	return {component, selectedTabId, set_selectedTabId};
+}
+
+export {useTabs};
--- apps/ycrv/components/list/GaugeListRow.tsx
+++ apps/ycrv/components/list/GaugeListRow.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button} from '@yearn-finance/web-lib/components/Button';
 import {toAddress} from '@yearn-finance/web-lib/utils/address';
 import {ImageWithFallback} from '@common/components/ImageWithFallback';
-import {formatPercent} from '@common/utils';
+import {isNumber} from '@common/utils/isNumber';
 
-import type {ReactElement} from 'react';
+import type {Dispatch, ReactElement, SetStateAction} from 'react';
+import type {TDict} from '@yearn-finance/web-lib/utils/types';
 import type {TCurveGauges} from '@common/types/curves';
 
-function	GaugeListRow({gauge}: {gauge: TCurveGauges}): ReactElement {
+type TGaugeListRow = {
+	gauge: TCurveGauges;
+	votes?: TDict<boolean>;
+	set_votes: Dispatch<SetStateAction<TDict<boolean>>>
+}
+
+function	GaugeListRow({gauge, votes, set_votes}: TGaugeListRow): ReactElement {
+	const [currentVotes, set_currentVotes] = useState<number>();
 	const locked = 0;
 
 	return (
@@ -28,28 +36,14 @@ function	GaugeListRow({gauge}: {gauge: TCurveGauges}): ReactElement {
 			</div>
 
 			<div className={'yearn--table-data-section'}>
-				<div className={'yearn--table-data-section-item justify-center md:col-span-1'} datatype={'number'}>
-					<label className={'yearn--table-data-section-item-label !font-aeonik'}>{'APY'}</label>
-					<p className={'yearn--table-data-section-item-value'}>
-						{formatPercent((0.1 || 0) * 100)}
-					</p>
-				</div>
-
-				<div className={'yearn--table-data-section-item justify-center md:col-span-2'} datatype={'number'}>
-					<label className={'yearn--table-data-section-item-label !font-aeonik'}>{'Current votes'}</label>
-					<p className={'yearn--table-data-section-item-value'}>
-						{Number(Math.random() * 1000).toFixed(0)}
-					</p>
-				</div>
-
 				<div className={'yearn--table-data-section-item justify-center md:col-span-2'} datatype={'number'}>
 					<label className={'yearn--table-data-section-item-label !font-aeonik'}>{'Your votes'}</label>
 					<p className={'yearn--table-data-section-item-value'}>
-						{Number(Math.random() * 1000).toFixed(0)}
+						{0}
 					</p>
 				</div>
 
-				<div className={'yearn--table-data-section-item md:col-span-2'} datatype={'number'}>
+				<div className={'yearn--table-data-section-item md:col-span-4'} datatype={'number'}>
 					<label className={'yearn--table-data-section-item-label !font-aeonik'}>{'Put your votes'}</label>
 					<p className={'yearn--table-data-section-item-value w-full text-neutral-900'}>
 						<div className={'flex h-10 items-center bg-neutral-200 p-2'}>
@@ -57,22 +51,37 @@ function	GaugeListRow({gauge}: {gauge: TCurveGauges}): ReactElement {
 								<input
 									id={'toAmount'}
 									className={'w-full cursor-default overflow-x-scroll border-none bg-transparent py-4 px-0 font-bold outline-none scrollbar-none'}
-									type={'text'}
+									type={'number'}
 									placeholder={'0'}
+									onChange={({target: {value}}): void => {
+										if (isNumber(+value)) {
+											set_currentVotes(+value);
+											set_votes((p): TDict<boolean> => ({
+												...p,
+												[gauge.gauge]: true
+											}));
+											return;
+										}
+										set_votes((p): TDict<boolean> => ({
+											...p,
+											[gauge.gauge]: false
+										}));
+									}}
+									value={currentVotes}
 								/>
 							</div>
 						</div>
 					</p>
 				</div>
 
-				<div className={'yearn--table-data-section-item md:col-span-1'} datatype={'number'}>
+				<div className={'yearn--table-data-section-item md:col-span-2'} datatype={'number'}>
 					<label className={'yearn--table-data-section-item-label !font-aeonik'}>{'Action'}</label>
 					<p className={`yearn--table-data-section-item-value ${locked === 0 ? 'text-neutral-400' : 'text-neutral-900'}`}>
 						<Button
-							onClick={(): void => alert('Not yet implemented!')}
+							onClick={(): void => alert(`${votes} for ${gauge.gauge}`)}
 							className={'w-full'}
 							isBusy={false}
-							isDisabled={false}>
+							isDisabled={!currentVotes}>
 							{'Vote'}
 						</Button>
 					</p>
