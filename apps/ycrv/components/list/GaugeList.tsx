a8b9435da57e2c0dec9a523ab2e0095f02f50e92
Karelian Pie <devkarelianpie@gmail.com>
2023-01-19 18:24:48 +0200

    feat: Add pagination

--- apps/ycrv/components/list/GaugeList.tsx
+++ apps/ycrv/components/list/GaugeList.tsx
@@ -1,8 +1,10 @@
 import React, {useCallback, useMemo, useState} from 'react';
+import ReactPaginate from 'react-paginate';
 import {useSessionStorage} from '@yearn-finance/web-lib/hooks/useSessionStorage';
 import performBatchedUpdates from '@yearn-finance/web-lib/utils/performBatchedUpdates';
 import ListHead from '@common/components/ListHead';
 import ListHero from '@common/components/ListHero';
+import IconChevron from '@common/icons/IconChevron';
 import {useSortGauges} from '@yCRV/hooks/useSortGauges';
 
 import {GaugeListEmpty} from './GaugeListEmpty';
@@ -25,6 +27,7 @@ function	GaugeList({gauges, isLoading}: TProps): ReactElement {
 	const 	[searchValue, set_searchValue] = useSessionStorage('yCRVGaugeSearchValue', '');
 	const	[sortBy, set_sortBy] = useState<TPossibleGaugesSortBy>('gauges');
 	const	[sortDirection, set_sortDirection] = useState<TPossibleGaugesSortDirection>('');
+	const	[itemOffset, set_itemOffset] = useState(0);
 
 	const	searchedGauges = useMemo((): TCurveGauges[] => {
 		if (searchValue === '') {
@@ -84,8 +87,67 @@ function	GaugeList({gauges, isLoading}: TProps): ReactElement {
 			);
 		}
 
-		return gauges;
-	}, [category, isSwitchEnabled, isVotesEmpty, searchValue, sortDirection, sortedGauges, votes]);
+		const ITEMS_PER_PAGE = 10;
+
+		const items = gauges.filter((g: ReactElement | null): boolean => !!g);
+
+		const handlePageClick = (event: {selected: number}): void => {
+			const newOffset = (event.selected * ITEMS_PER_PAGE) % items.length;
+			set_itemOffset(newOffset);
+		};
+
+		const endOffset = itemOffset + ITEMS_PER_PAGE;
+		const currentItems = items.slice(itemOffset, endOffset);
+		const pageCount = Math.ceil(items.length / ITEMS_PER_PAGE);
+
+		return (
+			<>
+				{currentItems}
+				<div className={'mt-4'}>
+					<div className={'flex items-center justify-between border-t border-neutral-300 p-4 sm:px-6'}>
+						<div className={'flex flex-1 justify-between sm:hidden'}>
+							<a
+								href={'#'}
+								className={'border-gray-300 text-gray-700 hover:bg-gray-50 relative inline-flex items-center rounded-md border  px-4 py-2 text-sm font-medium'}
+							>
+								{'Previous'}
+							</a>
+							<a
+								href={'#'}
+								className={'border-gray-300 text-gray-700 hover:bg-gray-50 relative ml-3 inline-flex items-center rounded-md border  px-4 py-2 text-sm font-medium'}
+							>
+								{'Next'}
+							</a>
+						</div>
+						<div className={'hidden sm:flex sm:flex-1 sm:items-center sm:justify-between'}>
+							<div>
+								<p className={'text-gray-700 text-sm'}>
+									{'Showing '}<span className={'font-medium'}>{endOffset - 9}</span>{' to '}<span className={'font-medium'}>{Math.min(endOffset, items.length)}</span>{' of'}{' '}
+									<span className={'font-medium'}>{items.length}</span> {'results'}
+								</p>
+							</div>
+							<ReactPaginate
+								className={'isolate inline-flex -space-x-px rounded-md shadow-sm'}
+								previousLinkClassName={'border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center rounded-l-md border  p-2 text-sm font-medium focus:z-20'}
+								nextLinkClassName={'border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center rounded-r-md border p-2 text-sm font-medium focus:z-20'}
+								breakLinkClassName={'border-gray-300 text-gray-700 relative inline-flex items-center border px-4 py-2 text-sm font-medium'}
+								activeLinkClassName={'border-gray-300 text-gray-500 hover:bg-gray-50 relative hidden items-center border px-4 py-2 text-sm font-medium focus:z-20 md:inline-flex bg-black'}
+								pageLinkClassName={'border-gray-300 text-gray-500 hover:bg-gray-50 relative inline-flex items-center border px-4 py-2 text-sm font-medium focus:z-20'}
+								disabledLinkClassName={'cursor-not-allowed bg-neutral-300'}
+								renderOnZeroPageCount={(): null => null}
+								breakLabel={'...'}
+								onPageChange={handlePageClick}
+								pageRangeDisplayed={5}
+								pageCount={pageCount}
+								nextLabel={<IconChevron className={'h-5 w-5 -rotate-90 transition-transform'} />}
+								previousLabel={<IconChevron className={'h-5 w-5 rotate-90 transition-transform'} />}
+							/>
+						</div>
+					</div>
+				</div>
+			</>
+		);
+	}, [category, isSwitchEnabled, isVotesEmpty, itemOffset, searchValue, sortDirection, sortedGauges, votes]);
 
 	return (
 		<div className={'relative col-span-12 flex w-full flex-col bg-neutral-100'}>
