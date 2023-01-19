a5f47fd83736ddcc976c7ba85ab53956e364032a
Karelian Pie <devkarelianpie@gmail.com>
2023-01-19 13:39:01 +0200

    feat: Add deposit/withdraw tx

--- apps/ycrv/hooks/useVLyCRV.ts
+++ apps/ycrv/hooks/useVLyCRV.ts
@@ -1,6 +1,6 @@
 import {useCallback} from 'react';
 import {Contract} from 'ethcall';
-import {BigNumber} from 'ethers';
+import {BigNumber, ethers} from 'ethers';
 import useSWR from 'swr';
 import {useWeb3} from '@yearn-finance/web-lib/contexts/useWeb3';
 import {toAddress} from '@yearn-finance/web-lib/utils/address';
@@ -13,9 +13,18 @@ type TUserInfo = {
 	lastVoteTime: number;
 }
 
+type TTransactionProps = {
+	provider: ethers.providers.Web3Provider;
+	amount: BigNumber;
+}
+
 type TUseVLyCRV = {
-	nextPeriod: number
-	userInfo: TUserInfo;
+	initialData: {
+		nextPeriod: number;
+		userInfo: TUserInfo;
+	};
+	deposit: (props: TTransactionProps) => Promise<boolean>;
+	withdraw: (props: TTransactionProps) => Promise<boolean>;
 };
 
 const DEFAULT_VLYCRV = {
@@ -27,11 +36,48 @@ const DEFAULT_VLYCRV = {
 	}
 };
 
+const VL_YCRV_CONTRACT = toAddress('0xCCBD4579495cD78280e4900CB482C8Edf2EC8336');
+
+async function deposit({provider, amount}: TTransactionProps): Promise<boolean> {
+	const signer = provider.getSigner();
+
+	try {
+		const contract = new ethers.Contract(VL_YCRV_CONTRACT, VLYCRV_ABI, signer);
+		const transaction = await contract.deposit(amount);
+		const transactionResult = await transaction.wait();
+		if (transactionResult.status === 0) {
+			throw new Error('Fail to perform transaction');
+		}
+
+		return true;
+	} catch(error) {
+		console.error(error);
+		return false;
+	}
+}
+
+async function withdraw({provider, amount}: TTransactionProps): Promise<boolean> {
+	const signer = provider.getSigner();
+
+	try {
+		const contract = new ethers.Contract(VL_YCRV_CONTRACT, VLYCRV_ABI, signer);
+		const transaction = await contract.withdraw(amount);
+		const transactionResult = await transaction.wait();
+		if (transactionResult.status === 0) {
+			throw new Error('Fail to perform transaction');
+		}
+
+		return true;
+	} catch(error) {
+		console.error(error);
+		return false;
+	}
+}
+
 export function useVLyCRV(): TUseVLyCRV {
 	const {provider, isActive, address} = useWeb3();
-	const VL_YCRV_CONTRACT = toAddress('0xCCBD4579495cD78280e4900CB482C8Edf2EC8336');
     
-	const fetcher = useCallback(async (): Promise<TUseVLyCRV> => {
+	const fetcher = useCallback(async (): Promise<TUseVLyCRV['initialData']> => {
 		if (!isActive || !provider) {
 			return DEFAULT_VLYCRV;
 		}
@@ -49,9 +95,9 @@ export function useVLyCRV(): TUseVLyCRV {
 		]) as [number, TUserInfo];
 
 		return {nextPeriod, userInfo};
-	}, [VL_YCRV_CONTRACT, address, isActive, provider]);
+	}, [address, isActive, provider]);
 
-	const {data} = useSWR<TUseVLyCRV>(isActive && provider ? 'vLyCRV' : null, fetcher);
+	const {data} = useSWR<TUseVLyCRV['initialData']>(isActive && provider ? 'vLyCRV' : null, fetcher);
 
-	return data ?? DEFAULT_VLYCRV;
+	return {initialData: data ?? DEFAULT_VLYCRV, deposit, withdraw};
 }
