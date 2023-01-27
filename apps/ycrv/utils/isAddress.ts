a34175dfa9b8ae2a55e67774c9c0d55fa860f899
Karelian Pie <devkarelianpie@gmail.com>
2023-01-27 16:18:53 +0200

    refactor: Use `isTAddress` from web-lib

--- apps/ycrv/utils/isAddress.ts
+++
@@ -1,5 +0,0 @@
-import type {TAddress} from '@yearn-finance/web-lib/utils/address';
-
-export function isAddress(value: string | TAddress): value is TAddress {
-	return /^0x([0-9a-f][0-9a-f])*$/i.test(value);
-}
