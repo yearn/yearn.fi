8f9c1d8d3343757476a46d90ce431450627745fa
Karelian Pie <devkarelianpie@gmail.com>
2023-01-27 10:01:14 +0200

    refactor: Use the `Tabs` component instead of hook

--- apps/common/components/Tabs.tsx
+++ apps/common/components/Tabs.tsx
@@ -22,10 +22,6 @@ type TTabsProps = {
 	className?: string,
 }
 
-/**
- * 
- * @deprecated Use the hook `useTabs()` instead.
- */
 function Tabs({items, className}: TTabsProps): ReactElement {
 	const [selectedTabId, set_selectedTabId] = useState(items[0]?.id);
 
