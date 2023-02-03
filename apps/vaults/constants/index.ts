72db2030bdc4575bbf41532169976e32c9058152
xgambitox <18649057+xgambitox@users.noreply.github.com>
2023-02-03 12:27:39 -0600

    refactor: hardcoded addresses

--- apps/vaults/constants/index.ts
+++ apps/vaults/constants/index.ts
@@ -2,4 +2,8 @@ import {toAddress} from '@yearn-finance/web-lib/utils/address';
 
 export const STAKING_REWARDS_SUPPORTED_CHAINS = [10];
 
+// TODO: hardcoded until a registry is deployed
+export const STAKING_REWARDS_ADDRESSES = [toAddress('0x1eC8BaAB7DBd6f5a02EfcAb711e765bF796d091c')];
+export const STAKING_REWARDS_REGISTRY_ADDRESS = toAddress();
+
 export const STAKING_REWARDS_ZAP_ADDRESS = toAddress(); // TODO: update once deployed
