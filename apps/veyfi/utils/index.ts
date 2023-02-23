4393a095aa06cfbe97541ffcee470af333d48386
xgambitox <18649057+xgambitox@users.noreply.github.com>
2023-02-23 13:54:27 -0600

    refactor: keyBy typing

--- apps/veyfi/utils/index.ts
+++ apps/veyfi/utils/index.ts
@@ -19,8 +19,7 @@ export function getVotingPower(lockAmount: BigNumber, unlockTime: TMilliseconds)
 }
 
 export const keyBy = <T1, T2 extends keyof T1 & string>(array: T1[], key: T2): TDict<T1 | undefined> => 
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	(array || []).reduce((r, x): TDict<T1> => ({...r, [(x as any)[key]]: x}), {});
+	(array || []).reduce((r, x): TDict<T1> => ({...r, [x[key] as unknown as string]: x}), {});
 
 export const isNumber = (value: unknown): boolean => !isNaN(value as number);
 
