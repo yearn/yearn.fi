f5bb55b99d241b3bcba28d8fb5f7dd25033583ac
xgambitox <18649057+xgambitox@users.noreply.github.com>
2023-02-24 11:17:41 -0600

    style: remove unnecessary unknowns

--- apps/common/components/Table.tsx
+++ apps/common/components/Table.tsx
@@ -74,7 +74,7 @@ function Table<T>({metadata, data, columns, initialSortBy, onRowClick}: TTablePr
 						onClick={(): void => onRowClick?.(item)}
 					>
 						{metadata.map(({key, label, className, fullWidth, columnSpan, format, transform}): ReactElement => {
-							const isNumber = !isNaN(item[key] as unknown as number);
+							const isNumber = !isNaN(item[key] as number);
 							const isZero = isNumber && Number(item[key]) === 0;
 							return (
 								<div key={`cell_${key}_${rowIndex}`} className={`flex h-8 flex-row items-center justify-between md:h-14 md:justify-end md:first:justify-start ${`md:col-span-${columnSpan ?? 1}`} ${className || ''}`}>
