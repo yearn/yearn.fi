6c5e565b4eee0d15054ecbfeeb2c6868b82184ea
Major <major@ycorpo.com>
2023-02-28 16:49:49 +0100

    fix: trailing slash

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -43,7 +43,7 @@ const WithLayout = memo(function WithLayout(props: AppProps): ReactElement {
 	return (
 		<PlausibleProvider
 			domain={'yearn.finance'}
-			customDomain={'https://analytics.yearn.finance'}
+			customDomain={'https://analytics.yearn.finance/'}
 			selfHosted>
 			<div id={'app'} className={'mx-auto mb-0 flex max-w-6xl font-aeonik'}>
 				<div className={'block min-h-[100vh] w-full'}>
