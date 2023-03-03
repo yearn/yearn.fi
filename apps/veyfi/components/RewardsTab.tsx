ac6c791a2361a94ce528592985476e213ac3c65d
xgambitox <18649057+xgambitox@users.noreply.github.com>
2023-03-03 16:13:09 -0600

    feat: implement base rewards functionality

--- apps/veyfi/components/RewardsTab.tsx
+++ apps/veyfi/components/RewardsTab.tsx
@@ -1,10 +1,64 @@
+import {useMemo, useState} from 'react';
+import {formatUnits} from 'ethers/lib/utils';
+import {useGauge} from '@veYFI/contexts/useGauge';
+import {useTransaction} from '@veYFI/hooks/useTransaction';
+import * as GaugeActions from '@veYFI/utils/actions/gauge';
+import {validateNetwork} from '@veYFI/utils/validations';
 import {Button} from '@yearn-finance/web-lib/components/Button';
+import {useWeb3} from '@yearn-finance/web-lib/contexts/useWeb3';
+import {toAddress} from '@yearn-finance/web-lib/utils/address';
+import {formatBN} from '@yearn-finance/web-lib/utils/format.bigNumber';
 import {AmountInput} from '@common/components/AmountInput';
 import {Dropdown} from '@common/components/Dropdown';
+import {useYearn} from '@common/contexts/useYearn';
 
+import type {BigNumber, ethers} from 'ethers';
 import type {ReactElement} from 'react';
+import type {TAddress} from '@yearn-finance/web-lib/utils/address';
+import type {TDropdownOption} from '@common/components/Dropdown';
 
 function RewardsTab(): ReactElement {
+	const [selectedGauge, set_selectedGauge] = useState<TDropdownOption>();
+	const {provider, address, isActive, chainID} = useWeb3();
+	const {gaugeAddresses, gaugesMap, positionsMap, refresh: refreshGauges} = useGauge();
+	const {vaults} = useYearn();
+	const refreshData = (): unknown => Promise.all([refreshGauges()]);
+	const [claim, claimStatus] = useTransaction(GaugeActions.claimRewards, refreshData);
+	const [claimAll, claimAllStatus] = useTransaction(GaugeActions.claimAllRewards, refreshData);
+	
+	const web3Provider = provider as ethers.providers.Web3Provider;
+	const userAddress = address as TAddress;
+	const selectedGaugeAddress = toAddress(selectedGauge?.key);
+	const selectedGaugeRewards = formatBN(positionsMap[selectedGaugeAddress]?.reward.balance);
+
+	const gaugeOptions = gaugeAddresses.map((address): TDropdownOption => {
+		const gauge = gaugesMap[address];
+		const vaultAddress = toAddress(gauge?.vaultAddress);
+		const vault = vaults[vaultAddress];
+
+		return {
+			key: address,
+			label: vault?.display_name ?? '',
+			icon: `${process.env.BASE_YEARN_ASSETS_URI}/1/${vaultAddress}/logo-128.png`
+		};
+	});
+
+	const gaugesRewards = useMemo((): BigNumber => {
+		return gaugeAddresses.reduce<BigNumber>((acc, address): BigNumber => {
+			return acc.add(formatBN(positionsMap[address]?.reward.balance));
+		}, formatBN(0));
+	}, [gaugeAddresses, positionsMap]);
+
+	const {isValid: isValidNetwork} = validateNetwork({supportedNetwork: 1, walletNetwork: chainID});
+
+	const onClaim = (): void => {
+		claim(web3Provider, userAddress, selectedGaugeAddress);
+	};
+
+	const onClaimAll = (): void => {
+		claimAll(web3Provider, userAddress, gaugeAddresses, true);
+	};
+
 	return (
 		<div className={'flex flex-col gap-6 md:gap-10'}>
 			<div className={'flex flex-col gap-4'}>
@@ -17,15 +71,15 @@ function RewardsTab(): ReactElement {
 				<div className={'grid grid-cols-1 gap-4 md:grid-cols-4'}>
 					<AmountInput
 						label={'Total unclaimed rewards (oYFI)'}
-						amount={0}
-						legend={'≈ $ 420.00'}
+						amount={formatUnits(gaugesRewards, 18)}
+						// legend={'≈ $ 420.00'} // TODO: oYFI price calcs
 						disabled
 					/>
 					<Button 
 						className={'w-full md:mt-7'}
-						onClick={(): void => undefined}
-						isBusy={false}
-						disabled={false}
+						onClick={onClaimAll}
+						disabled={!isActive || !isValidNetwork || gaugesRewards.eq(0)}
+						isBusy={claimAllStatus.loading}
 					>
 						{'Claim All'}
 					</Button>
@@ -42,19 +96,21 @@ function RewardsTab(): ReactElement {
 				<div className={'grid grid-cols-1 gap-4 md:grid-cols-4'}>
 					<Dropdown 
 						label={'Gauge'}
-						options={[]}
+						selected={selectedGauge}
+						options={gaugeOptions}
+						onChange={set_selectedGauge}
 					/>
 					<AmountInput
 						label={'Unclaimed rewards (oYFI)'}
-						amount={0}
-						legend={'≈ $ 420.00'}
+						amount={formatUnits(selectedGaugeRewards, 18)}
+						// legend={'≈ $ 420.00'} // TODO: oYFI price calcs
 						disabled
 					/>
 					<Button 
 						className={'w-full md:mt-7'}
-						onClick={(): void => undefined}
-						isBusy={false}
-						disabled={false}
+						onClick={onClaim}
+						disabled={!isActive || !isValidNetwork || selectedGaugeRewards.eq(0)}
+						isBusy={claimStatus.loading}
 					>
 						{'Claim'}
 					</Button>
