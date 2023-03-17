be13c26cc866a30ce47b4baf79ea39b9a14100e5
xgambitox <18649057+xgambitox@users.noreply.github.com>
2023-03-17 14:45:41 -0600

    fix: new types directory

--- apps/veyfi/components/GaugesTab.tsx
+++ apps/veyfi/components/GaugesTab.tsx
@@ -14,7 +14,7 @@ import {useYearn} from '@common/contexts/useYearn';
 
 import type {BigNumber, ethers} from 'ethers';
 import type {ReactElement} from 'react';
-import type {TAddress} from '@yearn-finance/web-lib/utils/address';
+import type {TAddress} from '@yearn-finance/web-lib/types';
 
 function GaugesTab(): ReactElement {
 	const [selectedGauge, set_selectedGauge] = useState('');
