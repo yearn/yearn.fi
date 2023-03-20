d7f7100145e8987be1c788c207164cd69c4d4a9d
Karelian Pie <devkarelianpie@gmail.com>
2023-03-20 14:38:43 +0200

    feat: Portals solver executor

--- apps/vaults/types/solvers.portals.ts
+++ apps/vaults/types/solvers.portals.ts
@@ -1,12 +1,5 @@
-export type TPortalsAPIResult = {
-	buyToken: string;
-	buyAmount: string;
-	minBuyAmount: string;
-	buyTokenDecimals: 0;
-};
-
-export type TPortalsResult = {
-	result: TPortalsAPIResult | undefined;
+export type TPortalsQuoteResult = {
+	result: TPortalEstimation | undefined;
 	isLoading: boolean;
 	error: Error | undefined;
 };
@@ -34,3 +27,41 @@ export type TPortalsApproval = {
 		};
 	};
 };
+
+export type TPortalTransaction = {
+	context: {
+		network: string;
+		protocolId: string;
+		sellToken: string;
+		sellAmount: string;
+		intermediateToken: string;
+		buyToken: string;
+		buyAmount: string;
+		minBuyAmount: string;
+		target: string;
+		partner: string;
+		takerAddress: string;
+		value: string;
+		gasLimit: string;
+	};
+	tx: {
+		to: string;
+		from: string;
+		data: string;
+		value: {
+			type: string;
+			hex: string;
+		};
+		gasLimit: {
+			type: string;
+			hex: string;
+		};
+	};
+}
+
+export type TPortalEstimation = {
+	buyToken: string;
+	buyAmount: string;
+	minBuyAmount: string;
+	buyTokenDecimals: number;
+}
