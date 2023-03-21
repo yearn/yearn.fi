93cda138c3fcbcca7ee4ed0133a5ae7f5b1744c8
xgambitox <18649057+xgambitox@users.noreply.github.com>
2023-03-21 13:57:51 -0600

    feat: add contexts to veyfi app wrapper

--- apps/veyfi/Wrapper.tsx
+++ apps/veyfi/Wrapper.tsx
@@ -5,6 +5,9 @@ import Meta from '@common/components/Meta';
 import {useCurrentApp} from '@common/hooks/useCurrentApp';
 import {variants} from '@common/utils/animations';
 
+import {GaugeContextApp} from './contexts/useGauge';
+import {OptionContextApp} from './contexts/useOption';
+
 import type {NextRouter} from 'next/router';
 import type {ReactElement} from 'react';
 
@@ -15,17 +18,21 @@ export default function Wrapper({children, router}: {children: ReactElement, rou
 		<>
 			<Meta meta={manifest} />
 			<VotingEscrowContextApp>
-				<AnimatePresence mode={'wait'}>
-					<motion.div
-						key={router.asPath}
-						initial={'initial'}
-						animate={'enter'}
-						exit={'exit'}
-						className={'my-0 h-full md:mb-0 md:mt-16'}
-						variants={variants}>
-						{children}
-					</motion.div>
-				</AnimatePresence>
+				<GaugeContextApp>
+					<OptionContextApp>
+						<AnimatePresence mode={'wait'}>
+							<motion.div
+								key={router.asPath}
+								initial={'initial'}
+								animate={'enter'}
+								exit={'exit'}
+								className={'my-0 h-full md:mb-0 md:mt-16'}
+								variants={variants}>
+								{children}
+							</motion.div>
+						</AnimatePresence>
+					</OptionContextApp>
+				</GaugeContextApp>
 			</VotingEscrowContextApp>
 		</>
 	);
