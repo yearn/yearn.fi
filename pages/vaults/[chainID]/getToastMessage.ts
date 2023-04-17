cc3df3818d96b7d0cc7c6ce49b3eb5421b273661
Karelian Pie <devkarelianpie@gmail.com>
2023-04-17 14:04:26 +0300

    refactor: Both names can be undefined

--- pages/vaults/[chainID]/getToastMessage.ts
+++ pages/vaults/[chainID]/getToastMessage.ts
@@ -1,4 +1,4 @@
-export function getToastMessage({vaultChainName, chainName}: {vaultChainName: string, chainName: string}): string {
+export function getToastMessage({vaultChainName, chainName}: {vaultChainName?: string, chainName?: string}): string {
 	if (vaultChainName && chainName) {
 		return `Please note, this Vault is on ${vaultChainName}. You're currently connected to ${chainName}.`;
 	}
