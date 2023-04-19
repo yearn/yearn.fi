083dea13cc050a273a9f2927730b97523360bffe
Karelian Pie <devkarelianpie@gmail.com>
2023-04-19 15:08:55 +0300

    refactor: Stying and auto close

--- apps/common/components/Popover.tsx
+++ apps/common/components/Popover.tsx
@@ -4,12 +4,16 @@ import {Popover as PopoverHeadlessUI, Transition} from '@headlessui/react';
 
 import type {ReactElement} from 'react';
 
+type TRequestType = 'bug' | 'feature';
+
 export function Popover(): ReactElement {
 	const [referenceElement, set_referenceElement] = useState<HTMLButtonElement | null>(null);
 	const [popperElement, set_popperElement] = useState<HTMLDivElement | null>(null);
 	const {styles, attributes} = usePopper(referenceElement, popperElement, {
-		modifiers: [{name: 'offset', options: {offset: [-100, 10]}}]
+		modifiers: [{name: 'offset', options: {offset: [-110, 10]}}]
 	});
+	const [type, set_type] = useState<TRequestType>('bug');
+	const [description, set_description] = useState<string>();
     
 	return (
 		<PopoverHeadlessUI className={'relative'}>
@@ -34,24 +38,45 @@ export function Popover(): ReactElement {
 					style={styles.popper}
 					{...attributes.popper}
 				>
-					<div className={'overflow-hidden rounded-lg bg-white p-5'}>
-						<div className={'space-y-2'}>
-							<label htmlFor={'type'}>{'type'}</label>
-							<div>
-								<select name={'type'} id={'type'}>
-									<option value={'bug'}>{'bug'}</option>
-									<option value={'feature'}>{'feature'}</option>
-								</select>
-							</div>
-							<label htmlFor={'description'}>{'description'}</label>
-							<div>
-								<textarea id={'description'}/>
-							</div>
-							<button>{'Submit'}</button>
+					{({close}): ReactElement => (
+						<div className={'flex flex-col space-y-2 overflow-hidden rounded-lg bg-white p-5'}>
+							<select
+								name={'type'}
+								id={'type'}
+								className={'text-xs'}
+								onChange={({target:{value}}): void => {
+									if (isRequestTypeKnown(value)) {
+										set_type(value);
+									}
+								}}
+							>
+								<option value={'bug'}>{'Bug'}</option>
+								<option value={'feature'}>{'Feature'}</option>
+							</select>
+							<textarea
+								id={'description'}
+								cols={30}
+								rows={4}
+								className={'text-xs'}
+								onChange={({target:{value}}): void => set_description(value)}
+							/>
+							<button
+								className={'relative h-8 cursor-pointer items-center justify-center border border-transparent bg-neutral-900 px-2 text-xs text-neutral-0 transition-all hover:bg-neutral-800'}
+								onClick={(): void => {
+									alert(`${type} submitted!\n\n${description}`);
+									close();
+								}}
+							>{'Submit'}
+							</button>
 						</div>
-					</div>
+					)}
+
 				</PopoverHeadlessUI.Panel>
 			</Transition>
 		</PopoverHeadlessUI>
 	);
+}
+
+function isRequestTypeKnown(type: string): type is TRequestType {
+	return type === 'bug' || type === 'feature';
 }
