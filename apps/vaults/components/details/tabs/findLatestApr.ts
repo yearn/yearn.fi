c0895c56ab4bcf55e7e759a9dd55c71f5141a583
Karelian Pie <devkarelianpie@gmail.com>
2023-05-10 16:42:49 +0300

    refactor: Coerce reports types

--- apps/vaults/components/details/tabs/findLatestApr.ts
+++ apps/vaults/components/details/tabs/findLatestApr.ts
@@ -1,17 +1,13 @@
 import type {TYDaemonReports} from '@vaults/schemas';
 
-export function findLatestApr(reports?: TYDaemonReports[]): number {
-	if (!reports?.length) {
+export function findLatestApr(reports: TYDaemonReports[]): number {
+	if (!reports.length) {
 		return 0;
 	}
 
 	const latestReport = reports.reduce((prev, curr): TYDaemonReports => {
-		return parseInt(prev.timestamp) > parseInt(curr.timestamp)
-			? prev
-			: curr;
+		return prev.timestamp > curr.timestamp ? prev : curr;
 	});
 
-	const apr = Number(latestReport.results[0]?.APR);
-
-	return isNaN(apr) ? 0 : apr * 100;
+	return latestReport.results[0].APR * 100;
 }
