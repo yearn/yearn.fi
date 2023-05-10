a1c866a255db8be431fe018b17e4ec197554d20f
Karelian Pie <devkarelianpie@gmail.com>
2023-05-10 13:02:39 +0300

    refactor: Infer type

--- apps/vaults/components/details/tabs/findLatestApr.ts
+++ apps/vaults/components/details/tabs/findLatestApr.ts
@@ -1,23 +1,17 @@
 import type {TYDaemonReports} from '@vaults/schemas';
 
-export type TYDaemonPartialReports = Pick<TYDaemonReports, 'timestamp'> & {
-	results: Pick<TYDaemonReports['results'][0], 'APR'>[];
-};
-
-export function findLatestApr(reports?: TYDaemonPartialReports[]): number {
+export function findLatestApr(reports?: TYDaemonReports[]): number {
 	if (!reports?.length) {
 		return 0;
 	}
 
-	const latestReport = reports.reduce(
-		(prev, curr): TYDaemonPartialReports => {
-			return parseInt(prev.timestamp) > parseInt(curr.timestamp)
-				? prev
-				: curr;
-		}
-	);
+	const latestReport = reports.reduce((prev, curr): TYDaemonReports => {
+		return parseInt(prev.timestamp) > parseInt(curr.timestamp)
+			? prev
+			: curr;
+	});
 
 	const apr = Number(latestReport.results[0]?.APR);
-    
+
 	return isNaN(apr) ? 0 : apr * 100;
 }
