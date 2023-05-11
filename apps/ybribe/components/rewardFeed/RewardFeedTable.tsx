800fa3940bf0b9fcee9063438521dc841c35220e
Karelian Pie <devkarelianpie@gmail.com>
2023-05-11 14:05:36 +0300

    feat: Add pagination

--- apps/ybribe/components/rewardFeed/RewardFeedTable.tsx
+++ apps/ybribe/components/rewardFeed/RewardFeedTable.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
+import ReactPaginate from 'react-paginate';
 import useSWR from 'swr';
 import {useSettings} from '@yearn-finance/web-lib/contexts/useSettings';
 import {baseFetcher} from '@yearn-finance/web-lib/utils/fetchers';
+import IconChevron from '@common/icons/IconChevron';
 import {yDaemonGaugeRewardsFeedSchema} from '@common/schemas';
 import {GaugeListEmpty} from '@yBribe/components/bribe/GaugeListEmpty';
 import {RewardFeedTableRow} from '@yBribe/components/rewardFeed/RewardFeedTableRow';
@@ -10,10 +12,10 @@ import type {ReactElement, ReactNode} from 'react';
 
 export function RewardFeedTable(): ReactElement | null {
 	const {settings: baseAPISettings} = useSettings();
-	const {data} = useSWR(
-		`${baseAPISettings.yDaemonBaseURI}/1/bribes/newRewardFeed`,
-		baseFetcher
-	);
+	const [itemOffset, set_itemOffset] = useState(0);
+
+	const endpoint = `${baseAPISettings.yDaemonBaseURI}/1/bribes/newRewardFeed`;
+	const {data} = useSWR(endpoint, baseFetcher);
 
 	if (!data) {
 		return null;
@@ -27,15 +29,66 @@ export function RewardFeedTable(): ReactElement | null {
 		return <GaugeListEmpty />;
 	}
 
-	const sortedFeed = result.data.sort((a, b): number => b.timestamp - a.timestamp) ;
+	const ITEMS_PER_PAGE = 15;
+	const items = result.data.sort((a, b): number => b.timestamp - a.timestamp) ;
+	const endOffset = itemOffset + ITEMS_PER_PAGE;
+	const currentItems = items.slice(itemOffset, endOffset);
+	const pageCount = Math.ceil(items.length / ITEMS_PER_PAGE);
+	const handlePageClick = (event: {selected: number}): void => {
+		const newOffset = (event.selected * ITEMS_PER_PAGE) % items.length;
+		set_itemOffset(newOffset);
+	};
 
 	return (
 		<>
-			{sortedFeed.filter(Boolean).map((item, index): ReactNode =>
+			{currentItems.filter(Boolean).map((item, index): ReactNode =>
 				<RewardFeedTableRow
 					key={`${index}-${item.txHash}_${item.briber}_${item.rewardToken}`}
 					currentRewardAdded={item} />
 			)}
+			<div className={'mt-4'}>
+				<div className={'flex flex-col justify-between gap-4 border-t border-neutral-300 p-4 sm:flex-row sm:items-center sm:px-6'}>
+					<div className={'flex flex-1 justify-between sm:hidden'}>
+						<a
+							href={'#'}
+							className={'border-gray-300 text-gray-700 hover:bg-gray-50 relative inline-flex items-center rounded-md border  px-4 py-2 text-sm font-medium'}
+						>
+							{'Previous'}
+						</a>
+						<a
+							href={'#'}
+							className={'border-gray-300 text-gray-700 hover:bg-gray-50 relative ml-3 inline-flex items-center rounded-md border  px-4 py-2 text-sm font-medium'}
+						>
+							{'Next'}
+						</a>
+					</div>
+					<div className={'hidden sm:flex sm:flex-1 sm:items-center sm:justify-between'}>
+						<div>
+							<p className={'text-gray-700 text-sm'}>
+								{'Showing '}<span className={'font-medium'}>{endOffset - (ITEMS_PER_PAGE - 1)}</span>{' to '}<span className={'font-medium'}>{Math.min(endOffset, items.length)}</span>{' of'}{' '}
+								<span className={'font-medium'}>{items.length}</span> {'results'}
+							</p>
+						</div>
+						<ReactPaginate
+							className={'isolate inline-flex -space-x-px rounded-md shadow-sm'}
+							pageLinkClassName={'text-gray-500 hover:bg-neutral-300 relative inline-flex items-center px-4 py-2 text-sm font-medium focus:z-20 border border-neutral-400'}
+							previousLinkClassName={'text-gray-500 hover:bg-neutral-300 relative inline-flex items-center p-2 text-sm font-medium focus:z-20 border border-neutral-400'}
+							nextLinkClassName={'text-gray-500 hover:bg-neutral-300 relative inline-flex items-center p-2 text-sm font-medium focus:z-20 border border-neutral-400'}
+							breakLinkClassName={'text-gray-700 relative inline-flex items-center px-4 py-2 text-sm font-medium hover:bg-neutral-300 border border-neutral-400'}
+							activeLinkClassName={'text-gray-500 hover:bg-neutral-300 relative hidden items-center px-4 py-2 text-sm font-medium focus:z-20 md:inline-flex bg-neutral-300'}
+							disabledLinkClassName={'cursor-not-allowed bg-neutral-100 hover:bg-neutral-100'}
+							disabledClassName={'text-neutral-300'}
+							renderOnZeroPageCount={(): null => null}
+							breakLabel={'...'}
+							onPageChange={handlePageClick}
+							pageRangeDisplayed={5}
+							pageCount={pageCount}
+							nextLabel={<IconChevron className={'h-5 w-5 -rotate-90 transition-transform'} />}
+							previousLabel={<IconChevron className={'h-5 w-5 rotate-90 transition-transform'} />}
+						/>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
