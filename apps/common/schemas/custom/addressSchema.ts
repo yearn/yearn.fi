533307215b73c31d62afb829abef268c66c1d983
Karelian Pie <devkarelianpie@gmail.com>
2023-05-23 09:43:10 +0300

    fix: Internal Server Error when invalid vaults

--- apps/common/schemas/custom/addressSchema.ts
+++ apps/common/schemas/custom/addressSchema.ts
@@ -2,7 +2,7 @@ import {z} from 'zod';
 
 import type {TAddress} from '@yearn-finance/web-lib/types';
 
-const ADDRESS_REGEX = new RegExp(/^0x[0-9a-f]{40}$/i);
+export const ADDRESS_REGEX = new RegExp(/^0x[0-9a-f]{40}$/i);
 
 // TODO Change in web-lib the address type to have the regex /^0x[0-9a-f]{40}$/i
 export const addressSchema = z.custom<TAddress>((val): boolean => {
