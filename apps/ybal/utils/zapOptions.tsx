ff3b2814dfbbd62eaadc8888fc89c9a7d68e5440
Major <major@ycorpo.com>
2023-05-24 11:30:41 +0200

    feat: update options from/to

--- apps/ybal/utils/zapOptions.tsx
+++ apps/ybal/utils/zapOptions.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import {BAL_TOKEN_ADDRESS, LPYBAL_TOKEN_ADDRESS, STYBAL_TOKEN_ADDRESS, YBAL_TOKEN_ADDRESS, ZAP_YEARN_YBAL_ADDRESS} from '@yearn-finance/web-lib/utils/constants';
+import {BAL_TOKEN_ADDRESS, BALWETH_TOKEN_ADDRESS, LPYBAL_TOKEN_ADDRESS, STYBAL_TOKEN_ADDRESS, WETH_TOKEN_ADDRESS, YBAL_TOKEN_ADDRESS, ZAP_YEARN_YBAL_ADDRESS} from '@yearn-finance/web-lib/utils/constants';
 
 import type {TDropdownOption} from '@common/types/types';
 
@@ -20,21 +20,36 @@ const	ZAP_OPTIONS_FROM: TDropdownOption[] = [
 		)
 	},
 	{
-		label: 'st-yBal',
-		symbol: 'st-yBal',
+		label: 'wETH',
+		symbol: 'wETH',
 		decimals: 18,
-		value: STYBAL_TOKEN_ADDRESS,
+		value: WETH_TOKEN_ADDRESS,
 		zapVia: ZAP_YEARN_YBAL_ADDRESS,
 		icon: (
 			<Image
-				alt={'st-yBal'}
+				alt={'wETH'}
 				width={24}
 				height={24}
-				src={`${process.env.BASE_YEARN_ASSETS_URI}/1/${STYBAL_TOKEN_ADDRESS}/logo-128.png`}
+				src={`${process.env.BASE_YEARN_ASSETS_URI}/1/${WETH_TOKEN_ADDRESS}/logo-128.png`} />
+		)
+	},
+	{
+		label: 'BAL/wETH',
+		symbol: 'BAL/wETH',
+		decimals: 18,
+		value: BALWETH_TOKEN_ADDRESS,
+		zapVia: ZAP_YEARN_YBAL_ADDRESS,
+		icon: (
+			<Image
+				alt={'BAL/wETH'}
+				width={24}
+				height={24}
+				src={`${process.env.BASE_YEARN_ASSETS_URI}/1/${BALWETH_TOKEN_ADDRESS}/logo-128.png`}
 				loading={'eager'}
 				priority />
 		)
-	}, {
+	},
+	{
 		label: 'yBal',
 		symbol: 'yBal',
 		decimals: 18,
@@ -46,6 +61,22 @@ const	ZAP_OPTIONS_FROM: TDropdownOption[] = [
 				width={24}
 				height={24}
 				src={`${process.env.BASE_YEARN_ASSETS_URI}/1/${YBAL_TOKEN_ADDRESS}/logo-128.png`}
+				loading={'eager'}
+				priority />
+		)
+	},
+	{
+		label: 'st-yBal',
+		symbol: 'st-yBal',
+		decimals: 18,
+		value: STYBAL_TOKEN_ADDRESS,
+		zapVia: ZAP_YEARN_YBAL_ADDRESS,
+		icon: (
+			<Image
+				alt={'st-yBal'}
+				width={24}
+				height={24}
+				src={`${process.env.BASE_YEARN_ASSETS_URI}/1/${STYBAL_TOKEN_ADDRESS}/logo-128.png`}
 				loading={'eager'}
 				priority />
 		)
@@ -70,21 +101,6 @@ const	ZAP_OPTIONS_FROM: TDropdownOption[] = [
 
 const	ZAP_OPTIONS_TO: TDropdownOption[] = [
 	{
-		label: 'st-yBal',
-		symbol: 'st-yBal',
-		decimals: 18,
-		value: STYBAL_TOKEN_ADDRESS,
-		zapVia: ZAP_YEARN_YBAL_ADDRESS,
-		icon: (
-			<Image
-				alt={'st-yBal'}
-				width={24}
-				height={24}
-				src={`${process.env.BASE_YEARN_ASSETS_URI}/1/${STYBAL_TOKEN_ADDRESS}/logo-128.png`}
-				loading={'eager'}
-				priority />
-		)
-	}, {
 		label: 'yBal',
 		symbol: 'yBal',
 		decimals: 18,
@@ -96,6 +112,22 @@ const	ZAP_OPTIONS_TO: TDropdownOption[] = [
 				width={24}
 				height={24}
 				src={`${process.env.BASE_YEARN_ASSETS_URI}/1/${YBAL_TOKEN_ADDRESS}/logo-128.png`}
+				loading={'eager'}
+				priority />
+		)
+	},
+	{
+		label: 'st-yBal',
+		symbol: 'st-yBal',
+		decimals: 18,
+		value: STYBAL_TOKEN_ADDRESS,
+		zapVia: ZAP_YEARN_YBAL_ADDRESS,
+		icon: (
+			<Image
+				alt={'st-yBal'}
+				width={24}
+				height={24}
+				src={`${process.env.BASE_YEARN_ASSETS_URI}/1/${STYBAL_TOKEN_ADDRESS}/logo-128.png`}
 				loading={'eager'}
 				priority />
 		)
