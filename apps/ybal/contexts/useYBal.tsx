3aafda4ff13d2b8cb5f29d0b2bd183142b6736f8
Major <major@ycorpo.com>
2023-05-26 09:34:58 +0200

    feat: new contract version

--- apps/ybal/contexts/useYBal.tsx
+++ apps/ybal/contexts/useYBal.tsx
@@ -18,7 +18,7 @@ import type {SWRResponse} from 'swr';
 import type {TDict} from '@yearn-finance/web-lib/types';
 import type {TYDaemonHarvests} from '@common/types/yearn';
 
-const LOCAL_ZAP_YEARN_YBAL_ADDRESS = toAddress('0x5E5713a0d915701F464DEbb66015adD62B2e6AE9');
+const LOCAL_ZAP_YEARN_YBAL_ADDRESS = toAddress('0x43cA9bAe8dF108684E5EAaA720C25e1b32B0A075');
 
 type THoldings = {
 	yBalSupply: BigNumber;
