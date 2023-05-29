5ab32821481652e9e2b41fbc7de8a9da7c138628
Major <major@ycorpo.com>
2023-05-29 10:46:51 +0200

    feat: ylock ui

--- pages/ycrv/holdings.tsx
+++ pages/ycrv/holdings.tsx
@@ -11,6 +11,7 @@ import {useYearn} from '@common/contexts/useYearn';
 import {useBalance} from '@common/hooks/useBalance';
 import {useTokenPrice} from '@common/hooks/useTokenPrice';
 import {getVaultAPY} from '@common/utils';
+import CardZap from '@yCRV/components/CardZap';
 import {Harvests} from '@yCRV/components/Harvests';
 import {useYCRV} from '@yCRV/contexts/useYCRV';
 import Wrapper from '@yCRV/Wrapper';
@@ -66,6 +67,265 @@ function	HeaderPosition(): ReactElement {
 				</b>
 			</div>
 		</Fragment>
+	);
+}
+
+function	ZapAndStats(): ReactElement {
+	const {balances} = useWallet();
+	const {holdings, styCRVMegaBoost, styCRVAPY} = useYCRV();
+	const {vaults} = useYearn();
+	const {curveWeeklyFees, cgPrices} = useCurve();
+
+	const lpCRVAPY = useMemo((): string => getVaultAPY(vaults, LPYCRV_TOKEN_ADDRESS), [vaults]);
+	const ycrvPrice = useTokenPrice(YCRV_TOKEN_ADDRESS);
+	const stycrvPrice = useTokenPrice(STYCRV_TOKEN_ADDRESS);
+	const lpycrvPrice = useTokenPrice(LPYCRV_TOKEN_ADDRESS);
+	const balanceOfStyCRV = useBalance(STYCRV_TOKEN_ADDRESS);
+	const balanceOfLpyCRV = useBalance(LPYCRV_TOKEN_ADDRESS);
+
+	const	formatBigNumberOver10K = useCallback((v: BigNumber): string => {
+		if (formatBN(v)?.gt(ethers.constants.WeiPerEther.mul(10000))) {
+			return formatAmount(formatToNormalizedValue(v || 0, 18), 0, 0)?.toString() ?? '';
+		}
+		return formatAmount(formatToNormalizedValue(v || 0, 18))?.toString() ?? '';
+	}, []);
+
+	const	formatNumberOver10K = useCallback((v: number): string => {
+		if (v >= 10000) {
+			return formatAmount(v, 0, 0)?.toString() ?? '';
+		}
+		return formatAmount(v)?.toString() ?? '';
+	}, []);
+
+	const	latestCurveFeesValue = useMemo((): number => {
+		const {weeklyFeesTable} = curveWeeklyFees;
+
+		if (!weeklyFeesTable) {
+			return 0;
+		}
+
+		if (weeklyFeesTable[0]?.rawFees > 0) {
+			return weeklyFeesTable[0].rawFees;
+		}
+
+		return weeklyFeesTable[1]?.rawFees || 0;
+	}, [curveWeeklyFees]);
+
+	const	currentVeCRVAPY = useMemo((): number => {
+		return (
+			latestCurveFeesValue / (
+				formatToNormalizedValue(formatBN(holdings?.veCRVTotalSupply), 18) * cgPrices?.['curve-dao-token']?.usd
+			) * 52 * 100
+		);
+	}, [holdings, latestCurveFeesValue, cgPrices]);
+
+	const	curveAdminFeePercent = useMemo((): number => {
+		return (currentVeCRVAPY * Number(holdings?.boostMultiplier) / 10000);
+	}, [holdings, currentVeCRVAPY]);
+
+
+	return (
+		<div className={'col-span-12 grid w-full grid-cols-12 gap-4'}>
+			<CardZap className={'col-span-12 md:col-span-8'} />
+			<div className={'col-span-12 flex flex-col gap-4 md:col-span-4'}>
+				<div className={'w-full bg-neutral-100 p-4'}>
+					<div className={'flex flex-row items-baseline justify-between pb-1'}>
+						<span className={'inline text-sm font-normal text-neutral-400'}>
+							{'Price/PEG: '}
+						</span>
+						<p
+							suppressHydrationWarning
+							className={'font-number text-sm text-neutral-900'}>
+							{`Price = $${(formatAmount(ycrvPrice || 0))} | Peg = ${(
+								holdings?.crvYCRVPeg ? (formatPercent(
+									(formatToNormalizedValue(holdings?.crvYCRVPeg, 18) + 0.0015) * 100)
+								): formatPercent(0)
+							)}`}
+						</p>
+					</div>
+
+					<div className={'flex flex-row items-baseline justify-between pb-1'}>
+						<span className={'inline text-sm font-normal text-neutral-400'}>
+							{'Yearn Treasury: '}
+						</span>
+						<p
+							suppressHydrationWarning
+							className={'font-number text-sm text-neutral-900'}>
+							{holdings?.treasury ? `${formatBigNumberOver10K(holdings?.treasury || 0)} ` : '- '}
+							<span className={'font-number text-neutral-600'}>{'veCRV'}</span>
+						</p>
+					</div>
+
+					<div className={'flex flex-row items-baseline justify-between pb-1'}>
+						<span className={'inline text-sm font-normal text-neutral-400'}>
+							{'Legacy system: '}
+						</span>
+						<p
+							suppressHydrationWarning
+							className={'font-number text-sm text-neutral-900'}>
+							{holdings?.legacy ? `${formatBigNumberOver10K(holdings?.legacy || 0)} ` : '- '}
+							<span className={'font-number text-neutral-600'}>{'yveCRV'}</span>
+						</p>
+					</div>
+				</div>
+
+				<div className={'w-full bg-neutral-100 p-4'}>
+					<div className={'flex flex-row items-center justify-between pb-3'}>
+						<b className={'text-neutral-900'}>
+							{'st-yCRV'}
+						</b>
+					</div>
+
+					<div className={'flex flex-row items-baseline justify-between pb-1'}>
+						<span className={'inline text-sm font-normal text-neutral-400'}>
+							{'My Balance: '}
+						</span>
+						<p
+							suppressHydrationWarning
+							className={'font-number text-sm text-neutral-900'}>
+							{formatNumberOver10K(balances[STYCRV_TOKEN_ADDRESS]?.normalized || 0)}
+						</p>
+					</div>
+					<div className={'flex flex-row items-center justify-between'}>
+						<span className={'inline text-sm font-normal text-neutral-400'}>
+							{'Value: '}
+						</span>
+						<p
+							suppressHydrationWarning
+							className={'font-number text-sm text-neutral-900'}>
+							{formatCounterValue(balanceOfStyCRV.normalized, stycrvPrice)}
+						</p>
+					</div>
+
+					<div className={'my-2 h-px w-full bg-neutral-200'} />
+
+					<div className={'flex flex-row items-center justify-between pb-1'}>
+						<span className={'mr-auto text-sm font-normal text-neutral-400'}>
+							{'APY: '}
+						</span>
+						<span className={'tooltip'}>
+							<b
+								suppressHydrationWarning
+								className={'font-number text-sm text-neutral-900'}>
+								{`${formatPercent(styCRVAPY ?? 0)}*`}
+							</b>
+							<span className={'tooltiptext'}>
+								<div className={'flex flex-col items-start justify-start text-left'}>
+									<p
+										suppressHydrationWarning
+										className={'font-number text-neutral-400 md:text-xxs'}>
+										{styCRVAPY ? `*${formatPercent(styCRVAPY)} APY: ` : `*${formatPercent(0)} APY: `}
+									</p>
+									<p
+										suppressHydrationWarning
+										className={'font-number text-neutral-400 md:text-xxs'}>
+										{`∙ ${curveAdminFeePercent ? formatPercent(curveAdminFeePercent) : formatPercent(0)} Curve Admin Fees (${formatAmount(Number(holdings?.boostMultiplier) / 10000)}x boost)`}
+									</p>
+									<p
+										suppressHydrationWarning
+										className={'font-number text-neutral-400 md:text-xxs'}>
+										{`∙ ${styCRVAPY && curveAdminFeePercent && styCRVMegaBoost ? formatAmount(styCRVAPY - (curveAdminFeePercent + (styCRVMegaBoost * 100)), 2, 2) : '0.00'}% Gauge Voting Bribes`}
+									</p>
+									<p
+										suppressHydrationWarning
+										className={'font-number text-neutral-400 md:text-xxs'}>
+										{`∙ ${styCRVMegaBoost ? formatPercent(styCRVMegaBoost * 100) : formatPercent(0)} Mega Boost`}
+									</p>
+								</div>
+							</span>
+						</span>
+					</div>
+					<div className={'flex flex-row items-center justify-between pb-1'}>
+						<span className={'inline text-sm font-normal text-neutral-400'}>
+							{'Total Assets: '}
+						</span>
+						<p
+							suppressHydrationWarning
+							className={'font-number text-sm text-neutral-900'}>
+							{holdings?.styCRVSupply ? formatCounterValue(
+								formatToNormalizedValue(holdings.styCRVSupply, 18),
+								ycrvPrice
+							) : formatAmount(0)}
+						</p>
+					</div>
+					<div className={'flex flex-row items-center justify-between pb-1'}>
+						<span className={'inline text-sm font-normal text-neutral-400'}>
+							{'yBal Deposits: '}
+						</span>
+						<p
+							suppressHydrationWarning
+							className={'font-number text-sm text-neutral-900'}>
+							{formatBigNumberOver10K(holdings?.styCRVSupply || 0)}
+						</p>
+					</div>
+				</div>
+
+				<div className={'w-full bg-neutral-100 p-4'}>
+					<div className={'flex flex-row items-center justify-between pb-3'}>
+						<b className={'text-neutral-900'}>
+							{'lp-yCRV'}
+						</b>
+					</div>
+
+					<div className={'flex flex-row items-baseline justify-between pb-1'}>
+						<span className={'inline text-sm font-normal text-neutral-400'}>
+							{'My Balance: '}
+						</span>
+						<p
+							suppressHydrationWarning
+							className={'font-number text-sm text-neutral-900'}>
+							{formatNumberOver10K(balances[LPYCRV_TOKEN_ADDRESS]?.normalized || 0)}
+						</p>
+					</div>
+					<div className={'flex flex-row items-center justify-between'}>
+						<span className={'inline text-sm font-normal text-neutral-400'}>
+							{'Value: '}
+						</span>
+						<p
+							suppressHydrationWarning
+							className={'font-number text-sm text-neutral-900'}>
+							{formatCounterValue(balanceOfLpyCRV.normalized, lpycrvPrice)}
+						</p>
+					</div>
+
+					<div className={'my-2 h-px w-full bg-neutral-200'} />
+
+					<div className={'flex flex-row items-center justify-between pb-1'}>
+						<span className={'mr-auto text-sm font-normal text-neutral-400'}>
+							{'APY: '}
+						</span>
+						<b
+							suppressHydrationWarning
+							className={'font-number text-sm text-neutral-900'}>
+							{lpCRVAPY ? `${(lpCRVAPY || '').replace('APY', '')}` : `${formatPercent(0)}`}
+						</b>
+					</div>
+					<div className={'flex flex-row items-center justify-between pb-1'}>
+						<span className={'inline text-sm font-normal text-neutral-400'}>
+							{'Total Assets: '}
+						</span>
+						<p
+							suppressHydrationWarning
+							className={'font-number text-sm text-neutral-900'}>
+							{holdings?.lpyCRVSupply ? formatCounterValue(
+								formatToNormalizedValue(holdings.lpyCRVSupply, 18),
+								lpycrvPrice
+							) : formatAmount(0)}
+						</p>
+					</div>
+					<div className={'flex flex-row items-center justify-between pb-1'}>
+						<span className={'inline text-sm font-normal text-neutral-400'}>
+							{'yBal Deposits: '}
+						</span>
+						<p
+							suppressHydrationWarning
+							className={'font-number text-sm text-neutral-900'}>
+							{formatBigNumberOver10K(holdings?.lpyCRVSupply || 0)}
+						</p>
+					</div>
+				</div>
+			</div>
+		</div>
 	);
 }
 
@@ -127,7 +387,8 @@ function	Holdings(): ReactElement {
 
 			<HeaderPosition />
 
-			<div className={'col-span-12 flex w-full flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4'}>
+			<ZapAndStats />
+			<div className={'col-span-12 hidden w-full flex-col space-y-4 md:flex-row md:space-y-0 md:space-x-4'}>
 				<div className={'w-full bg-neutral-100 p-6 md:w-[412px] md:min-w-[412px]'}>
 					<div className={'grid w-full gap-6 md:col-span-5'}>
 						<div>
