e37b4b232450704cbad9a73898fc710dc6b38813
Major <major@ycorpo.com>
2023-05-29 14:27:54 +0200

    feat: migrate common

--- apps/common/hooks/useBalance.tsx
+++ apps/common/hooks/useBalance.tsx
@@ -4,16 +4,15 @@ import {toAddress} from '@yearn-finance/web-lib/utils/address';
 import {toNormalizedBN} from '@yearn-finance/web-lib/utils/format.bigNumber';
 import {useWallet} from '@common/contexts/useWallet';
 
-import type {TBalanceData} from '@yearn-finance/web-lib/hooks/types';
 import type {TAddress, TDict} from '@yearn-finance/web-lib/types';
+import type {TBalanceData} from '@yearn-finance/web-lib/types/hooks';
 
 function	useBalance(
 	address: string | TAddress,
 	source?: TDict<TBalanceData>
 ): TBalanceData {
-	const	{balances, balancesNonce} = useWallet();
-
-	const	balance = useMemo((): TBalanceData => {
+	const {balances, balancesNonce} = useWallet();
+	const balance = useMemo((): TBalanceData => {
 		balancesNonce; // remove warning, force deep refresh
 		if (source) {
 			return source?.[toAddress(address)] || toNormalizedBN(0);
