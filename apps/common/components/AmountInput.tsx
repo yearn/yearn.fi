de9c525cd1d621da5a70ab32cecd2779ab08dd40
Major <major@ycorpo.com>
2023-05-30 11:50:54 +0200

    fix: undo test void

--- apps/common/components/AmountInput.tsx
+++ apps/common/components/AmountInput.tsx
@@ -52,13 +52,13 @@ function AmountInput({
 					min={0}
 					aria-label={label}
 					value={displayedAmount}
-					onChange={onAmountChange ? (e) => onAmountChange(e.target.value) : undefined}
+					onChange={onAmountChange ? (e): void => onAmountChange(e.target.value) : undefined}
 					placeholder={loading ? '' : placeholder ?? '0'}
 					disabled={disabled}
 				/>
 				<Renderable shouldRender={!!maxAmount && !disabled}>
 					<button
-						onClick={onMaxClick ? () => onMaxClick() : undefined}
+						onClick={onMaxClick ? (): void => onMaxClick() : undefined}
 						className={'absolute right-2 ml-2 h-6 cursor-pointer border-none bg-neutral-900 px-2 py-1 text-xs text-neutral-0 transition-colors hover:bg-neutral-700'}>
 						{'Max'}
 					</button>
