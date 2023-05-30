f3c3058c4128de2d7f64378317961ddd189d68d7
Major <major@ycorpo.com>
2023-05-30 12:01:43 +0200

    fix: undo change for upcoming pr

--- apps/vaults/utils/actions/stakingRewards.tsx
+++ apps/vaults/utils/actions/stakingRewards.tsx
@@ -5,6 +5,7 @@ import {handleTx} from '@yearn-finance/web-lib/utils/web3/transaction';
 import STAKING_REWARDS_ABI from '../abi/stakingRewards.abi';
 import STAKING_REWARDS_ZAP_ABI from '../abi/stakingRewardsZap.abi';
 
+import type {BigNumber} from 'ethers';
 import type {TAddress} from '@yearn-finance/web-lib/types';
 import type {TTxResponse} from '@yearn-finance/web-lib/utils/web3/transaction';
 
@@ -12,7 +13,7 @@ export async function stake(
 	provider: ethers.providers.JsonRpcProvider,
 	accountAddress: TAddress,
 	stakingAddress: TAddress,
-	amount: bigint
+	amount: BigNumber
 ): Promise<TTxResponse> {
 	const signer = provider.getSigner(accountAddress);
 	const stakingRewardsContract = new ethers.Contract(stakingAddress, STAKING_REWARDS_ABI, signer);
@@ -43,7 +44,7 @@ export async function depositAndStake(
 	provider: ethers.providers.JsonRpcProvider,
 	accountAddress: TAddress,
 	vaultAddress: TAddress,
-	amount: bigint
+	amount: BigNumber
 ): Promise<TTxResponse> {
 	const signer = provider.getSigner(accountAddress);
 	const stakingRewardsZapContract = new ethers.Contract(STAKING_REWARDS_ZAP_ADDRESS, STAKING_REWARDS_ZAP_ABI, signer);
