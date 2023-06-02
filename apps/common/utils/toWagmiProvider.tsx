7bb9c343ca5d441f349f78834fadae8cf7606117
Major <major@ycorpo.com>
2023-06-02 09:46:33 +0200

    feat: wagmi handleTX init

--- apps/common/utils/toWagmiProvider.tsx
+++ apps/common/utils/toWagmiProvider.tsx
@@ -1,12 +1,16 @@
+import {captureException} from '@sentry/nextjs';
+import {prepareWriteContract, waitForTransaction, writeContract} from '@wagmi/core';
 import {toAddress} from '@yearn-finance/web-lib/utils/address';
 import {ETH_TOKEN_ADDRESS, ZERO_ADDRESS} from '@yearn-finance/web-lib/utils/constants';
 import {isTAddress} from '@yearn-finance/web-lib/utils/isTAddress';
+import {defaultTxStatus} from '@yearn-finance/web-lib/utils/web3/transaction';
 import {assert} from '@common/utils/assert';
 
+import type {Abi, BaseError, SimulateContractParameters} from 'viem';
 import type {Connector} from 'wagmi';
 import type {TAddress} from '@yearn-finance/web-lib/types';
-import type {defaultTxStatus} from '@yearn-finance/web-lib/utils/web3/transaction';
-import type {GetWalletClientResult} from '@wagmi/core';
+import type {TTxResponse} from '@yearn-finance/web-lib/utils/web3/transaction';
+import type {GetWalletClientResult, WalletClient} from '@wagmi/core';
 
 export type TWagmiProviderContract = {
 	walletClient: GetWalletClientResult,
@@ -37,4 +41,58 @@ export function assertAddress(addr: string | TAddress | undefined, name?: string
 	assert(isTAddress(addr), `${name || 'Address'} provided is invalid`);
 	assert(toAddress(addr) !== ZERO_ADDRESS, `${name || 'Address'} is 0x0`);
 	assert(toAddress(addr) !== ETH_TOKEN_ADDRESS, `${name || 'Address'} is 0xE`);
+}
+
+type TPrepareWriteContractConfig<
+	TAbi extends Abi | readonly unknown[] = Abi,
+	TFunctionName extends string = string,
+	TChainId extends number = number,
+	TWalletClient extends WalletClient = WalletClient
+> = Omit<SimulateContractParameters<TAbi, TFunctionName>, 'chain' | 'address'> & {
+	chainId?: TChainId | number
+	walletClient?: TWalletClient | null
+	address: TAddress | undefined
+}
+export async function handleTx<
+	TAbi extends Abi | readonly unknown[],
+	TFunctionName extends string,
+	TChainId extends number,
+	TWalletClient extends WalletClient = WalletClient
+>(
+	args: TWriteTransaction,
+	props: TPrepareWriteContractConfig<TAbi, TFunctionName, TChainId, TWalletClient>
+): Promise<TTxResponse> {
+	args.statusHandler?.({...defaultTxStatus, pending: true});
+	const wagmiProvider = await toWagmiProvider(args.connector);
+
+	//Some extra assertions
+	assertAddress(props.address, 'contractAddress');
+	assertAddress(wagmiProvider.address, 'userAddress');
+	try {
+		const config = await prepareWriteContract({
+			...wagmiProvider,
+			...props as TPrepareWriteContractConfig,
+			address: props.address
+		});
+		const {hash} = await writeContract(config.request);
+		const receipt = await waitForTransaction({chainId: wagmiProvider.chainId, hash});
+		if (receipt.status === 'success') {
+			args.statusHandler?.({...defaultTxStatus, success: true});
+		} else if (receipt.status === 'reverted') {
+			args.statusHandler?.({...defaultTxStatus, error: true});
+		}
+		return ({isSuccessful: receipt.status === 'success', receipt});
+	} catch (error) {
+		console.error(error);
+		const errorAsBaseError = error as BaseError;
+		if (process.env.NODE_ENV === 'production') {
+			captureException(errorAsBaseError);
+		}
+		args.statusHandler?.({...defaultTxStatus, error: true});
+		return ({isSuccessful: false, error: errorAsBaseError || ''});
+	} finally {
+		setTimeout((): void => {
+			args.statusHandler?.({...defaultTxStatus});
+		}, 3000);
+	}
 }
