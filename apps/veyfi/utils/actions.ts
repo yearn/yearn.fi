72c40f54b61a31eb9d59db6c8ec47132e5cce6a9
Major <major@ycorpo.com>
2023-06-02 10:29:30 +0200

    feat: migrate actions

--- apps/veyfi/utils/actions.ts
+++ apps/veyfi/utils/actions.ts
@@ -1,238 +1,148 @@
-import {captureException} from '@sentry/nextjs';
 import VEYFI_ABI from '@veYFI/utils/abi/veYFI.abi';
-import {prepareWriteContract, waitForTransaction, writeContract} from '@wagmi/core';
+import {prepareWriteContract} from '@wagmi/core';
+import {toAddress} from '@yearn-finance/web-lib/utils/address';
 import {defaultTxStatus} from '@yearn-finance/web-lib/utils/web3/transaction';
 import {assert} from '@common/utils/assert';
-import {assertAddress, toWagmiProvider} from '@common/utils/toWagmiProvider';
+import {assertAddress, handleTx} from '@common/utils/toWagmiProvider';
 
-import type {BaseError} from 'viem';
 import type {TTxResponse} from '@yearn-finance/web-lib/utils/web3/transaction';
 import type {TWriteTransaction} from '@common/utils/toWagmiProvider';
 
 /* 🔵 - Yearn Finance **********************************************************
-** lock is a _WRITE_ function that locks funds in the veYFI contract in
+** lockVeYFI is a _WRITE_ function that locks funds in the veYFI contract in
 ** exchange of some voting power.
 **
 ** @app - veYFI
 ** @param amount - The amount of the underlying asset to deposit.
 ** @param time - The amount of time to lock the funds for.
 ******************************************************************************/
-type TLock = TWriteTransaction & {
+type TLockVeYFI = TWriteTransaction & {
 	amount: bigint;
 	time: bigint
 };
-export async function lock(props: TLock): Promise<TTxResponse> {
-	assertAddress(props.contractAddress, 'contractAddress');
+export async function lockVeYFI(props: TLockVeYFI): Promise<TTxResponse> {
+	assert(props.connector, 'No connector');
 	assert(props.time > 0n, 'Time is 0');
 	assert(props.amount > 0n, 'Amount is 0');
 
-	props.statusHandler?.({...defaultTxStatus, pending: true});
-	const wagmiProvider = await toWagmiProvider(props.connector);
+	const signerAddress = await props.connector.getAccount();
+	assertAddress(signerAddress, 'signerAddress');
 
-	try {
-		const config = await prepareWriteContract({
-			...wagmiProvider,
-			address: props.contractAddress,
-			abi: VEYFI_ABI,
-			functionName: 'modify_lock',
-			args: [props.amount, props.time, wagmiProvider.address]
-		});
-		const {hash} = await writeContract(config.request);
-		const receipt = await waitForTransaction({chainId: wagmiProvider.chainId, hash});
-		if (receipt.status === 'success') {
-			props.statusHandler?.({...defaultTxStatus, success: true});
-		} else if (receipt.status === 'reverted') {
-			props.statusHandler?.({...defaultTxStatus, error: true});
-		}
-		return ({isSuccessful: receipt.status === 'success', receipt});
-	} catch (error) {
-		console.error(error);
-		const errorAsBaseError = error as BaseError;
-		captureException(errorAsBaseError);
-		props.statusHandler?.({...defaultTxStatus, error: true});
-		return ({isSuccessful: false, error: errorAsBaseError || ''});
-	} finally {
-		setTimeout((): void => {
-			props.statusHandler?.({...defaultTxStatus});
-		}, 3000);
-	}
-
+	return await handleTx(props, {
+		address: props.contractAddress,
+		abi: VEYFI_ABI,
+		functionName: 'modify_lock',
+		args: [props.amount, props.time, signerAddress]
+	});
 }
 
 /* 🔵 - Yearn Finance **********************************************************
-** increaseLockAmount is a _WRITE_ function that increases the amount of funds
-** locked in the veYFI contract in exchange of some voting power.
+** increaseVeYFILockAmount is a _WRITE_ function that increases the amount of
+** funds locked in the veYFI contract in exchange of some voting power.
 **
 ** @app - veYFI
 ** @param amount - The amount of the underlying asset to deposit.
 ******************************************************************************/
-type TIncreaseLockAmount = TWriteTransaction & {
+type TIncreaseVeYFILockAmount = TWriteTransaction & {
 	amount: bigint;
 };
-export async function increaseLockAmount(props: TIncreaseLockAmount): Promise<TTxResponse> {
-	assertAddress(props.contractAddress, 'contractAddress');
+export async function increaseVeYFILockAmount(props: TIncreaseVeYFILockAmount): Promise<TTxResponse> {
+	assert(props.connector, 'No connector');
 	assert(props.amount > 0n, 'Amount is 0');
 
-	props.statusHandler?.({...defaultTxStatus, pending: true});
-	const wagmiProvider = await toWagmiProvider(props.connector);
+	const signerAddress = await props.connector.getAccount();
+	assertAddress(signerAddress, 'signerAddress');
 
-	try {
-		const config = await prepareWriteContract({
-			...wagmiProvider,
-			address: props.contractAddress,
-			abi: VEYFI_ABI,
-			functionName: 'modify_lock',
-			args: [props.amount, 0n, wagmiProvider.address]
-		});
-		const {hash} = await writeContract(config.request);
-		const receipt = await waitForTransaction({chainId: wagmiProvider.chainId, hash});
-		if (receipt.status === 'success') {
-			props.statusHandler?.({...defaultTxStatus, success: true});
-		} else if (receipt.status === 'reverted') {
-			props.statusHandler?.({...defaultTxStatus, error: true});
-		}
-		return ({isSuccessful: receipt.status === 'success', receipt});
-	} catch (error) {
-		console.error(error);
-		const errorAsBaseError = error as BaseError;
-		captureException(errorAsBaseError);
-		props.statusHandler?.({...defaultTxStatus, error: true});
-		return ({isSuccessful: false, error: errorAsBaseError || ''});
-	} finally {
-		setTimeout((): void => {
-			props.statusHandler?.({...defaultTxStatus});
-		}, 3000);
-	}
+	return await handleTx(props, {
+		address: props.contractAddress,
+		abi: VEYFI_ABI,
+		functionName: 'modify_lock',
+		args: [props.amount, 0n, signerAddress]
+	});
 }
 
 /* 🔵 - Yearn Finance **********************************************************
-** extendLockTime is a _WRITE_ function that increases the amount of time funds
+** extendVeYFILockTime is a _WRITE_ function that increases the amount of time funds
 ** are locked in the veYFI contract in exchange of some voting power.
 **
 ** @app - veYFI
 ** @param time - The amount of time to lock the funds for.
 ******************************************************************************/
-type TExtendLockTime = TWriteTransaction & {
+type TExtendVeYFILockTime = TWriteTransaction & {
 	time: bigint;
 };
-export async function extendLockTime(props: TExtendLockTime): Promise<TTxResponse> {
-	assertAddress(props.contractAddress, 'contractAddress');
+export async function extendVeYFILockTime(props: TExtendVeYFILockTime): Promise<TTxResponse> {
+	assert(props.connector, 'No connector');
 	assert(props.time > 0n, 'Time is 0');
 
-	props.statusHandler?.({...defaultTxStatus, pending: true});
-	const wagmiProvider = await toWagmiProvider(props.connector);
+	const signerAddress = await props.connector.getAccount();
+	assertAddress(signerAddress, 'signerAddress');
 
+	return await handleTx(props, {
+		address: props.contractAddress,
+		abi: VEYFI_ABI,
+		functionName: 'modify_lock',
+		args: [0n, props.time, signerAddress]
+	});
+}
+
+/* 🔵 - Yearn Finance **********************************************************
+** getVeYFIWithdrawPenalty is a _READ_ function that simulates a withdrawal from
+** the veYFI contract and returns the penalty to be paid.
+**
+** @app - veYFI
+******************************************************************************/
+type TGetVeYFIWithdrawPenalty = TWriteTransaction;
+export async function getVeYFIWithdrawPenalty(props: TGetVeYFIWithdrawPenalty): Promise<bigint> {
 	try {
-		const config = await prepareWriteContract({
-			...wagmiProvider,
-			address: props.contractAddress,
+		const {result} = await prepareWriteContract({
+			address: toAddress(props.contractAddress),
 			abi: VEYFI_ABI,
-			functionName: 'modify_lock',
-			args: [0n, props.time, wagmiProvider.address]
+			functionName: 'withdraw'
 		});
-		const {hash} = await writeContract(config.request);
-		const receipt = await waitForTransaction({chainId: wagmiProvider.chainId, hash});
-		if (receipt.status === 'success') {
-			props.statusHandler?.({...defaultTxStatus, success: true});
-		} else if (receipt.status === 'reverted') {
-			props.statusHandler?.({...defaultTxStatus, error: true});
-		}
-		return ({isSuccessful: receipt.status === 'success', receipt});
+		return result.penalty;
 	} catch (error) {
-		console.error(error);
-		const errorAsBaseError = error as BaseError;
-		captureException(errorAsBaseError);
-		props.statusHandler?.({...defaultTxStatus, error: true});
-		return ({isSuccessful: false, error: errorAsBaseError || ''});
-	} finally {
-		setTimeout((): void => {
-			props.statusHandler?.({...defaultTxStatus});
-		}, 3000);
+		return 0n;
 	}
 }
 
 /* 🔵 - Yearn Finance **********************************************************
-** withdrawUnlocked is a _WRITE_ function that withdraws unlocked funds from
+** withdrawUnlockedVeYFI is a _WRITE_ function that withdraws unlocked funds from
 ** the veYFI contract.
 ** Note: will fail if there is a penalty to be paid.
 **
 ** @app - veYFI
 ******************************************************************************/
-type TWithdrawUnlocked = TWriteTransaction;
-export async function withdrawUnlocked(props: TWithdrawUnlocked): Promise<TTxResponse> {
-	assertAddress(props.contractAddress, 'contractAddress');
-
+type TWithdrawUnlockedVeYFI = TWriteTransaction;
+export async function withdrawUnlockedVeYFI(props: TWithdrawUnlockedVeYFI): Promise<TTxResponse> {
 	props.statusHandler?.({...defaultTxStatus, pending: true});
-	const wagmiProvider = await toWagmiProvider(props.connector);
-
-	try {
-		const config = await prepareWriteContract({
-			...wagmiProvider,
-			address: props.contractAddress,
-			abi: VEYFI_ABI,
-			functionName: 'withdraw'
-		});
-		if (config.result.penalty > 0n) {
-			throw new Error('Tokens are not yet unlocked');
-		}
-		const {hash} = await writeContract(config.request);
-		const receipt = await waitForTransaction({chainId: wagmiProvider.chainId, hash});
-		if (receipt.status === 'success') {
-			props.statusHandler?.({...defaultTxStatus, success: true});
-		} else if (receipt.status === 'reverted') {
-			props.statusHandler?.({...defaultTxStatus, error: true});
-		}
-		return ({isSuccessful: receipt.status === 'success', receipt});
-	} catch (error) {
-		console.error(error);
-		const errorAsBaseError = error as BaseError;
-		captureException(errorAsBaseError);
+	const penalty = await getVeYFIWithdrawPenalty(props);
+	if (penalty > 0n) {
 		props.statusHandler?.({...defaultTxStatus, error: true});
-		return ({isSuccessful: false, error: errorAsBaseError || ''});
-	} finally {
 		setTimeout((): void => {
 			props.statusHandler?.({...defaultTxStatus});
 		}, 3000);
+		return ({isSuccessful: false, error: new Error('Tokens are not yet unlocked')});
 	}
+
+	return await handleTx(props, {
+		address: props.contractAddress,
+		abi: VEYFI_ABI,
+		functionName: 'withdraw'
+	});
 }
 
 /* 🔵 - Yearn Finance **********************************************************
-** withdrawLocked is a _WRITE_ function that withdraws locked funds from the
+** withdrawLockedVeYFI is a _WRITE_ function that withdraws locked funds from the
 ** veYFI contract.
 **
 ** @app - veYFI
 ******************************************************************************/
-type TWithdrawLocked = TWriteTransaction;
-export async function withdrawLocked(props: TWithdrawLocked): Promise<TTxResponse> {
-	assertAddress(props.contractAddress, 'contractAddress');
-
-	props.statusHandler?.({...defaultTxStatus, pending: true});
-	const wagmiProvider = await toWagmiProvider(props.connector);
-
-	try {
-		const config = await prepareWriteContract({
-			...wagmiProvider,
-			address: props.contractAddress,
-			abi: VEYFI_ABI,
-			functionName: 'withdraw'
-		});
-		const {hash} = await writeContract(config.request);
-		const receipt = await waitForTransaction({chainId: wagmiProvider.chainId, hash});
-		if (receipt.status === 'success') {
-			props.statusHandler?.({...defaultTxStatus, success: true});
-		} else if (receipt.status === 'reverted') {
-			props.statusHandler?.({...defaultTxStatus, error: true});
-		}
-		return ({isSuccessful: receipt.status === 'success', receipt});
-	} catch (error) {
-		console.error(error);
-		const errorAsBaseError = error as BaseError;
-		captureException(errorAsBaseError);
-		props.statusHandler?.({...defaultTxStatus, error: true});
-		return ({isSuccessful: false, error: errorAsBaseError || ''});
-	} finally {
-		setTimeout((): void => {
-			props.statusHandler?.({...defaultTxStatus});
-		}, 3000);
-	}
+type TWithdrawLockedVeYFI = TWriteTransaction;
+export async function withdrawLockedVeYFI(props: TWithdrawLockedVeYFI): Promise<TTxResponse> {
+	return await handleTx(props, {
+		address: props.contractAddress,
+		abi: VEYFI_ABI,
+		functionName: 'withdraw'
+	});
 }
--- apps/ybribe/utils/actions.ts
+++ apps/ybribe/utils/actions.ts
@@ -1,14 +1,8 @@
-import {captureException} from '@sentry/nextjs';
-import {prepareWriteContract, waitForTransaction, writeContract} from '@wagmi/core';
-import {CURVE_BRIBE_V2_ADDRESS, CURVE_BRIBE_V3_ADDRESS} from '@yearn-finance/web-lib/utils/constants';
-import {defaultTxStatus} from '@yearn-finance/web-lib/utils/web3/transaction';
+import {CURVE_BRIBE_V3_ADDRESS} from '@yearn-finance/web-lib/utils/constants';
 import {assert} from '@common/utils/assert';
-import {assertAddress, toWagmiProvider} from '@common/utils/toWagmiProvider';
-import CURVE_BRIBE_V2_ABI from '@yBribe/utils/abi/curveBribeV2.abi';
+import {assertAddress, handleTx} from '@common/utils/toWagmiProvider';
+import CURVE_BRIBE_V3_ABI from '@yBribe/utils/abi/curveBribeV3.abi';
 
-import CURVE_BRIBE_V3_ABI from './abi/curveBribeV3.abi';
-
-import type {BaseError} from 'viem';
 import type {TAddress} from '@yearn-finance/web-lib/types';
 import type {TTxResponse} from '@yearn-finance/web-lib/utils/web3/transaction';
 import type {TWriteTransaction} from '@common/utils/toWagmiProvider';
@@ -16,7 +10,6 @@ import type {TWriteTransaction} from '@common/utils/toWagmiProvider';
 /* 🔵 - Yearn Finance **********************************************************
 ** claimReward is a _WRITE_ function that claims the rewards from the yBribe
 ** contract.
-** The correct function for V2 or V3 should be used.
 **
 ** @app - yBribe
 ** @param gaugeAddress - The address of the gauge to claim rewards from.
@@ -26,80 +19,21 @@ type TClaimReward = TWriteTransaction & {
 	gaugeAddress: TAddress | undefined;
 	tokenAddress: TAddress | undefined;
 };
-export async function claimRewardV2(props: TClaimReward): Promise<TTxResponse> {
-	assertAddress(CURVE_BRIBE_V2_ADDRESS, 'CURVE_BRIBE_V2_ADDRESS');
-	assertAddress(props.contractAddress, 'contractAddress');
-	assertAddress(props.gaugeAddress, 'gaugeAddress');
-	assertAddress(props.tokenAddress, 'tokenAddress');
-
-	props.statusHandler?.({...defaultTxStatus, pending: true});
-	const wagmiProvider = await toWagmiProvider(props.connector);
-
-	try {
-		const config = await prepareWriteContract({
-			...wagmiProvider,
-			address: CURVE_BRIBE_V2_ADDRESS,
-			abi: CURVE_BRIBE_V2_ABI,
-			functionName: 'claim_reward',
-			args: [wagmiProvider.address, props.gaugeAddress, props.tokenAddress]
-		});
-		const {hash} = await writeContract(config.request);
-		const receipt = await waitForTransaction({chainId: wagmiProvider.chainId, hash});
-		if (receipt.status === 'success') {
-			props.statusHandler?.({...defaultTxStatus, success: true});
-		} else if (receipt.status === 'reverted') {
-			props.statusHandler?.({...defaultTxStatus, error: true});
-		}
-		return ({isSuccessful: receipt.status === 'success', receipt});
-	} catch (error) {
-		console.error(error);
-		const errorAsBaseError = error as BaseError;
-		captureException(errorAsBaseError);
-		props.statusHandler?.({...defaultTxStatus, error: true});
-		return ({isSuccessful: false, error: errorAsBaseError || ''});
-	} finally {
-		setTimeout((): void => {
-			props.statusHandler?.({...defaultTxStatus});
-		}, 3000);
-	}
-}
-
 export async function claimRewardV3(props: TClaimReward): Promise<TTxResponse> {
+	assert(props.connector, 'No connector');
 	assertAddress(CURVE_BRIBE_V3_ADDRESS, 'CURVE_BRIBE_V3_ADDRESS');
-	assertAddress(props.contractAddress, 'contractAddress');
 	assertAddress(props.gaugeAddress, 'gaugeAddress');
 	assertAddress(props.tokenAddress, 'tokenAddress');
 
-	props.statusHandler?.({...defaultTxStatus, pending: true});
-	const wagmiProvider = await toWagmiProvider(props.connector);
+	const signerAddress = await props.connector.getAccount();
+	assertAddress(signerAddress, 'signerAddress');
 
-	try {
-		const config = await prepareWriteContract({
-			...wagmiProvider,
-			address: CURVE_BRIBE_V3_ADDRESS,
-			abi: CURVE_BRIBE_V3_ABI,
-			functionName: 'claim_reward_for',
-			args: [wagmiProvider.address, props.gaugeAddress, props.tokenAddress]
-		});
-		const {hash} = await writeContract(config.request);
-		const receipt = await waitForTransaction({chainId: wagmiProvider.chainId, hash});
-		if (receipt.status === 'success') {
-			props.statusHandler?.({...defaultTxStatus, success: true});
-		} else if (receipt.status === 'reverted') {
-			props.statusHandler?.({...defaultTxStatus, error: true});
-		}
-		return ({isSuccessful: receipt.status === 'success', receipt});
-	} catch (error) {
-		console.error(error);
-		const errorAsBaseError = error as BaseError;
-		captureException(errorAsBaseError);
-		props.statusHandler?.({...defaultTxStatus, error: true});
-		return ({isSuccessful: false, error: errorAsBaseError || ''});
-	} finally {
-		setTimeout((): void => {
-			props.statusHandler?.({...defaultTxStatus});
-		}, 3000);
-	}
+	return await handleTx(props, {
+		address: CURVE_BRIBE_V3_ADDRESS,
+		abi: CURVE_BRIBE_V3_ABI,
+		functionName: 'claim_reward_for',
+		args: [signerAddress, props.gaugeAddress, props.tokenAddress]
+	});
 }
 
 
@@ -119,40 +53,14 @@ type TAddReward = TWriteTransaction & {
 };
 export async function addReward(props: TAddReward): Promise<TTxResponse> {
 	assertAddress(CURVE_BRIBE_V3_ADDRESS, 'CURVE_BRIBE_V3_ADDRESS');
-	assertAddress(props.contractAddress, 'contractAddress');
 	assertAddress(props.gaugeAddress, 'gaugeAddress');
 	assertAddress(props.tokenAddress, 'tokenAddress');
 	assert(props.amount > 0n, 'Amount must be greater than 0');
 
-	props.statusHandler?.({...defaultTxStatus, pending: true});
-	const wagmiProvider = await toWagmiProvider(props.connector);
-
-	try {
-		const config = await prepareWriteContract({
-			...wagmiProvider,
-			address: CURVE_BRIBE_V3_ADDRESS,
-			abi: CURVE_BRIBE_V3_ABI,
-			functionName: 'add_reward_amount',
-			args: [props.gaugeAddress, props.tokenAddress, props.amount]
-		});
-		const {hash} = await writeContract(config.request);
-		const receipt = await waitForTransaction({chainId: wagmiProvider.chainId, hash});
-		if (receipt.status === 'success') {
-			props.statusHandler?.({...defaultTxStatus, success: true});
-		} else if (receipt.status === 'reverted') {
-			props.statusHandler?.({...defaultTxStatus, error: true});
-		}
-		return ({isSuccessful: receipt.status === 'success', receipt});
-	} catch (error) {
-		console.error(error);
-		const errorAsBaseError = error as BaseError;
-		captureException(errorAsBaseError);
-		props.statusHandler?.({...defaultTxStatus, error: true});
-		return ({isSuccessful: false, error: errorAsBaseError || ''});
-	} finally {
-		setTimeout((): void => {
-			props.statusHandler?.({...defaultTxStatus});
-		}, 3000);
-	}
-
+	return await handleTx(props, {
+		address: CURVE_BRIBE_V3_ADDRESS,
+		abi: CURVE_BRIBE_V3_ABI,
+		functionName: 'add_reward_amount',
+		args: [props.gaugeAddress, props.tokenAddress, props.amount]
+	});
 }
