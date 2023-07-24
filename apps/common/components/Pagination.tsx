aba60579add358b0158f9efa9a9732f4dac2ada2
Chef Karelian <78794805+karelianpie@users.noreply.github.com>
2023-07-24 13:22:16 +0300

    feat: Add button role
    Co-authored-by: Major <90963895+Majorfi@users.noreply.github.com>

--- apps/common/components/Pagination.tsx
+++ apps/common/components/Pagination.tsx
@@ -19,6 +19,7 @@ export function Pagination(props: TProps): ReactElement {
 		<>
 			<div className={'flex flex-1 justify-between sm:hidden'}>
 				<a
+					role={'button'}
 					href={'#'}
 					className={'border-gray-300 text-gray-700 hover:bg-gray-50 relative inline-flex items-center rounded-md border px-4 py-2 text-sm font-medium'}
 				>
