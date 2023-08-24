73048f877c954dec77931becbf0c9b0ab916ee94
Karelian Pie <devkarelianpie@gmail.com>
2023-08-24 11:00:48 +0300

    docs: Add jsdoc for useFeatureFlag

--- apps/common/hooks/useFeatureFlag.tsx
+++ apps/common/hooks/useFeatureFlag.tsx
@@ -1,5 +1,19 @@
 import {useSearchParams} from 'next/navigation';
 
+/**
+ * Determines if a specific feature flag is enabled based on the URL's search parameters.
+ *
+ * @function
+ * @name useFeatureFlag
+ * @param {string} flag - The name of the feature flag to check for.
+ * @returns {[boolean]} A boolean value inside an array indicating if the provided feature flag is enabled (`true` if enabled, `false` otherwise).
+ * 
+ * @example
+ * const [isFeatureEnabled] = useFeatureFlag('newFeature');
+ * if (isFeatureEnabled) {
+ *    // render or implement the new feature
+ * }
+ */
 export function useFeatureFlag(flag: string): [boolean] {
 	const searchParams = useSearchParams();
 
