68e7cd32f98cc3ba8dcbcb44534279a1c9166068
Major <major@ycorpo.com>
2023-10-21 10:46:56 +0200

    fix: strategies not being displayed with debt > 0

--- apps/vaults/components/graphs/GraphForStrategyReports.tsx
+++ apps/vaults/components/graphs/GraphForStrategyReports.tsx
@@ -64,85 +64,90 @@ export function GraphForStrategyReports({
 	}
 
 	return (
-		<ResponsiveContainer
-			width={'100%'}
-			height={height}>
-			<LineChart
-				margin={{top: 0, right: -28, bottom: 0, left: 0}}
-				data={strategyData}>
-				<Line
-					className={'text-primary-600'}
-					type={'step'}
-					strokeWidth={2}
-					dataKey={'value'}
-					stroke={'currentcolor'}
-					dot={false}
-					activeDot={(e: any): ReactElement => {
-						e.className = `${e.className} activeDot`;
-						delete e.dataKey;
-						return <circle {...e}></circle>;
-					}}
-				/>
-				<XAxis
-					dataKey={'name'}
-					hide
-				/>
-				<YAxis
-					orientation={'right'}
-					hide={false}
-					tick={(e): ReactElement => {
-						const {
-							payload: {value}
-						} = e;
-						e.fill = '#5B5B5B';
-						e.className = 'text-xxs md:text-xs font-number z-10 ';
-						e.alignmentBaseline = 'middle';
-						delete e.verticalAnchor;
-						delete e.visibleTicksCount;
-						delete e.tickFormatter;
-						const formatedValue = formatPercent(value);
-						return <text {...e}>{formatedValue}</text>;
-					}}
-				/>
-				<Tooltip
-					content={(e): ReactElement => {
-						const {active: isTooltipActive, payload, label} = e;
-						if (!isTooltipActive || !payload) {
-							return <></>;
-						}
-						if (payload.length > 0) {
-							const [{value, payload: innerPayload}] = payload;
-							const {gain, loss} = innerPayload;
-							const diff = toBigInt(gain) - toBigInt(loss);
-							const normalizedDiff = formatToNormalizedValue(diff, vaultDecimals);
+		<>
+			<p className={'text-neutral-600'}>{'Historical APR'}</p>
+			<div className={'mt-4 flex flex-row border-b border-l border-neutral-300'}>
+				<ResponsiveContainer
+					width={'100%'}
+					height={height}>
+					<LineChart
+						margin={{top: 0, right: -28, bottom: 0, left: 0}}
+						data={strategyData}>
+						<Line
+							className={'text-primary-600'}
+							type={'step'}
+							strokeWidth={2}
+							dataKey={'value'}
+							stroke={'currentcolor'}
+							dot={false}
+							activeDot={(e: any): ReactElement => {
+								e.className = `${e.className} activeDot`;
+								delete e.dataKey;
+								return <circle {...e}></circle>;
+							}}
+						/>
+						<XAxis
+							dataKey={'name'}
+							hide
+						/>
+						<YAxis
+							orientation={'right'}
+							hide={false}
+							tick={(e): ReactElement => {
+								const {
+									payload: {value}
+								} = e;
+								e.fill = '#5B5B5B';
+								e.className = 'text-xxs md:text-xs font-number z-10 ';
+								e.alignmentBaseline = 'middle';
+								delete e.verticalAnchor;
+								delete e.visibleTicksCount;
+								delete e.tickFormatter;
+								const formatedValue = formatPercent(value);
+								return <text {...e}>{formatedValue}</text>;
+							}}
+						/>
+						<Tooltip
+							content={(e): ReactElement => {
+								const {active: isTooltipActive, payload, label} = e;
+								if (!isTooltipActive || !payload) {
+									return <></>;
+								}
+								if (payload.length > 0) {
+									const [{value, payload: innerPayload}] = payload;
+									const {gain, loss} = innerPayload;
+									const diff = toBigInt(gain) - toBigInt(loss);
+									const normalizedDiff = formatToNormalizedValue(diff, vaultDecimals);
 
-							return (
-								<div className={'recharts-tooltip'}>
-									<div className={'mb-4'}>
-										<p className={'text-xs'}>{formatDate(label)}</p>
-									</div>
-									<div className={'flex flex-row items-center justify-between'}>
-										<p className={'text-xs text-neutral-600'}>{'APR'}</p>
-										<b className={'font-number text-xs font-bold text-neutral-900'}>
-											{formatPercent(Number(value))}
-										</b>
-									</div>
-									<div className={'flex flex-row items-center justify-between'}>
-										<p className={'text-xs text-neutral-600'}>
-											{normalizedDiff > 0 ? 'Gain' : 'Loss'}
-										</p>
-										<b
-											className={
-												'font-number text-xs font-bold text-neutral-900'
-											}>{`${formatAmount(normalizedDiff)} ${vaultTicker}`}</b>
-									</div>
-								</div>
-							);
-						}
-						return <div />;
-					}}
-				/>
-			</LineChart>
-		</ResponsiveContainer>
+									return (
+										<div className={'recharts-tooltip'}>
+											<div className={'mb-4'}>
+												<p className={'text-xs'}>{formatDate(label)}</p>
+											</div>
+											<div className={'flex flex-row items-center justify-between'}>
+												<p className={'text-xs text-neutral-600'}>{'APR'}</p>
+												<b className={'font-number text-xs font-bold text-neutral-900'}>
+													{formatPercent(Number(value))}
+												</b>
+											</div>
+											<div className={'flex flex-row items-center justify-between'}>
+												<p className={'text-xs text-neutral-600'}>
+													{normalizedDiff > 0 ? 'Gain' : 'Loss'}
+												</p>
+												<b
+													className={
+														'font-number text-xs font-bold text-neutral-900'
+													}>{`${formatAmount(normalizedDiff)} ${vaultTicker}`}</b>
+											</div>
+										</div>
+									);
+								}
+								return <div />;
+							}}
+						/>
+					</LineChart>
+				</ResponsiveContainer>
+			</div>
+		</>
 	);
 }
