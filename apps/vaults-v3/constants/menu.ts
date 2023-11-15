6db1f4e5d9dfa51534fd8adce985de9363c6eeca
Karelian Pie <devkarelianpie@gmail.com>
2023-11-15 10:54:15 +0200

    feat: Add about page menu

--- apps/vaults-v3/constants/menu.ts
+++ apps/vaults-v3/constants/menu.ts
@@ -1,1 +1,4 @@
-export const VAULTS_V3_MENU = [{path: '/vaults-v3', label: 'Vaults'}];
+export const VAULTS_V3_MENU = [
+	{path: '/vaults-v3', label: 'Vaults'},
+	{path: '/vaults-v3/about', label: 'About'}
+];
