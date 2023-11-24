51bf2ae57e62540b95d65e29c1caed7e4750e639
Majorfi <major-tom.eth@pm.me>
2023-11-24 12:57:21 +0100

    fix: invalid perf fee on v3

--- apps/vaults-v3/components/details/tabs/VaultDetailsAbout.tsx
+++ apps/vaults-v3/components/details/tabs/VaultDetailsAbout.tsx
@@ -43,6 +43,8 @@ export function VaultDetailsAbout({currentVault}: {currentVault: TYDaemonVault})
 		return 'Sorry, we don\'t have a description for this asset right now. But did you know the correct word for a blob of toothpaste is a "nurdle". Fascinating! We\'ll work on updating the asset description, but at least you learnt something interesting. Catch ya later nurdles.';
 	}
 
+	console.log(apr);
+
 	return (
 		<div className={'grid grid-cols-1 gap-4 p-4 md:grid-cols-12 md:gap-10 md:p-8'}>
 			<div className={'col-span-12 w-full pr-0 md:col-span-7 md:pr-28'}>
@@ -85,12 +87,12 @@ export function VaultDetailsAbout({currentVault}: {currentVault: TYDaemonVault})
 						<div className={'mt-4 grid grid-cols-4 gap-8'}>
 							<YearnFeesLineItem label={'Management'}>
 								<b className={'font-number text-xl text-neutral-900'}>
-									{formatPercent((apr.fees.management || 0) / 100, 0)}
+									{formatPercent((apr.fees.management || 0) * 100, 0)}
 								</b>
 							</YearnFeesLineItem>
 							<YearnFeesLineItem label={'Performance'}>
 								<b className={'font-number text-xl text-neutral-900'}>
-									{formatPercent((apr.fees.performance || 0) / 100, 0)}
+									{formatPercent((apr.fees.performance || 0) * 100, 0)}
 								</b>
 							</YearnFeesLineItem>
 							{currentVault.category === 'Velodrome' || currentVault.category === 'Aerodrome' ? (
@@ -158,12 +160,12 @@ export function VaultDetailsAboutCard({currentVault}: {currentVault: TYDaemonVau
 
 						<YearnFeesLineItem label={'Management'}>
 							<b className={'font-number text-xl text-neutral-900'}>
-								{formatPercent((apr.fees.management || 0) / 100, 0)}
+								{formatPercent((apr.fees.management || 0) * 100, 0)}
 							</b>
 						</YearnFeesLineItem>
 						<YearnFeesLineItem label={'Performance'}>
 							<b className={'font-number text-xl text-neutral-900'}>
-								{formatPercent((apr.fees.performance || 0) / 100, 0)}
+								{formatPercent((apr.fees.performance || 0) * 100, 0)}
 							</b>
 						</YearnFeesLineItem>
 						{currentVault.category === 'Velodrome' || currentVault.category === 'Aerodrome' ? (
