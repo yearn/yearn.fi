7b1aaad7e1f307f66a85ba35456c68a71de95e02
0xkorin <117271860+0xkorin@users.noreply.github.com>
2023-12-04 10:55:43 +0100

    Update redemption contract address (#450)

--- apps/veyfi/utils/constants.ts
+++ apps/veyfi/utils/constants.ts
@@ -6,7 +6,7 @@ import type {TWeeks} from '@yearn-finance/web-lib/utils/time';
 export const VEYFI_CHAIN_ID = 1;
 
 export const VEYFI_REGISTRY_ADDRESS: TAddress = toAddress(''); // TODO: update once deployed
-export const VEYFI_OPTIONS_ADDRESS = toAddress('0x2fBa208E1B2106d40DaA472Cb7AE0c6C7EFc0224');
+export const VEYFI_OPTIONS_ADDRESS = toAddress('0x7dC3A74F0684fc026f9163C6D5c3C99fda2cf60a');
 export const VEYFI_DYFI_ADDRESS = toAddress('0x41252E8691e964f7DE35156B68493bAb6797a275');
 export const VEYFI_ADDRESS = toAddress('0x90c1f9220d90d3966FbeE24045EDd73E1d588aD5');
 export const VEYFI_POSITION_HELPER_ADDRESS = toAddress('0x5A70cD937bA3Daec8188E937E243fFa43d6ECbe8');
