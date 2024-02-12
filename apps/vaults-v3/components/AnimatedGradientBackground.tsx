2cd0ce25ae61af8b429a43a03d4fc9ddf4574270
Majorfi <major@ycorpo.com>
2024-02-12 11:15:09 +0100

    fix: animate

--- apps/vaults-v3/components/AnimatedGradientBackground.tsx
+++ apps/vaults-v3/components/AnimatedGradientBackground.tsx
@@ -1,18 +1,11 @@
-import {motion} from 'framer-motion';
 import {cl} from '@builtbymom/web3/utils';
 
 import type {ReactElement} from 'react';
 
 export function AnimatedGradientBackgroundForV3(): ReactElement {
 	return (
-		<motion.div
-			transition={{duration: 10, delay: 0, repeat: Infinity, ease: 'linear'}}
-			animate={{
-				background: [
-					`linear-gradient(0deg, #D21162 24.91%, #2C3DA6 99.66%)`,
-					`linear-gradient(360deg, #D21162 24.91%, #2C3DA6 99.66%)`
-				]
-			}}
+		<div
+			style={{background: 'linear-gradient(0deg, #D21162 24.91%, #2C3DA6 99.66%)'}}
 			className={cl('absolute inset-0', 'pointer-events-none')}
 		/>
 	);
