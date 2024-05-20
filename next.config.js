3f84b4bff723c094bf8c0deb385354f798eccdcd
Majorfi <major@ycorpo.com>
2024-05-20 10:47:21 +0200

    feat: RPC

--- next.config.js
+++ next.config.js
@@ -116,6 +116,19 @@ const config = {
 		/* 🔵 - Yearn Finance **************************************************
 		 ** Config over the RPC
 		 **********************************************************************/
+		RPC_URI_FOR_1: process.env.RPC_URI_FOR_1,
+		RPC_URI_FOR_10: process.env.RPC_URI_FOR_10,
+		RPC_URI_FOR_137: process.env.RPC_URI_FOR_137,
+		RPC_URI_FOR_252: process.env.RPC_URI_FOR_252,
+		RPC_URI_FOR_288: process.env.RPC_URI_FOR_288,
+		RPC_URI_FOR_8453: process.env.RPC_URI_FOR_8453,
+		RPC_URI_FOR_42161: process.env.RPC_URI_FOR_42161,
+		RPC_URI_FOR_42170: process.env.RPC_URI_FOR_42170,
+		RPC_URI_FOR_56288: process.env.RPC_URI_FOR_56288,
+		RPC_URI_FOR_81457: process.env.RPC_URI_FOR_81457,
+		RPC_URI_FOR_111188: process.env.RPC_URI_FOR_111188,
+
+		// Old setup
 		WEB_SOCKET_URL: {
 			1: process.env.WS_URL_MAINNET,
 			10: process.env.WS_URL_OPTIMISM,
@@ -130,6 +143,7 @@ const config = {
 			250: process.env.RPC_URL_FANTOM,
 			42161: process.env.RPC_URL_ARBITRUM
 		},
+
 		ALCHEMY_KEY: process.env.ALCHEMY_KEY,
 		ALCHEMY_API_KEY: process.env.ALCHEMY_API_KEY,
 		INFURA_PROJECT_ID: process.env.INFURA_PROJECT_ID,
