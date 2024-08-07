494b258859d8a07ea45c8d2073698550e642d495
Karlen9 <misakyankarlen@gmail.com>
2024-08-07 12:30:24 +0400

    fix: small fix

--- apps/common/components/AppsCarousel.tsx
+++ apps/common/components/AppsCarousel.tsx
@@ -9,12 +9,12 @@ export const AppsCarousel = forwardRef((props: {apps: TApp[]}, ref: ForwardedRef
 		<section className={'left-0 -mx-1 w-full md:absolute'}>
 			<div
 				className={
-					'pointer-events-none absolute left-0 top-0 z-10 h-full w-1/6 bg-gradient-to-r from-gray-900 to-transparent'
+					'pointer-events-none absolute left-0 top-0 z-30 h-full w-1/6 bg-gradient-to-r from-gray-900 to-transparent'
 				}
 			/>
 			<div
 				className={
-					'pointer-events-none absolute right-0 top-0 z-10 h-full w-1/5 bg-gradient-to-l from-gray-900 to-transparent'
+					'pointer-events-none absolute right-0 top-0 z-30 h-full w-1/5 bg-gradient-to-l from-gray-900 to-transparent'
 				}
 			/>
 			<div
