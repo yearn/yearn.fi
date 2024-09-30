fcc6b09b42f1ee72c80ae84a1b67a704f9dffc66
Karlen9 <misakyankarlen@gmail.com>
2024-09-30 13:46:29 +0300

    chore: type fix

--- apps/common/components/Sidebar.tsx
+++ apps/common/components/Sidebar.tsx
@@ -67,7 +67,7 @@ export function Sidebar(props: TSidebarProps): ReactElement {
 							href={tab.route === '/' ? tab.route : `/home/${tab.route}`}
 							key={tab.route}>
 							<div className={'flex size-6 items-center justify-center'}>
-								{iconsDict[tab.route as '/' | 'apps' | 'vaults' | 'yearn-x' | 'integrations']}
+								{iconsDict[tab.route as '/' | 'community-apps' | 'vaults' | 'yearn-x' | 'integrations']}
 							</div>
 							<p>{tab.title}</p>
 						</Link>
