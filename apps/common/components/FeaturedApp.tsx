7be886537c6e3e7437d8aeff8e54b619d08fd378
Daniil Polienko <donyaschefer@gmail.com>
2024-10-16 18:53:49 +0300

    fix: animation bug

--- apps/common/components/FeaturedApp.tsx
+++ apps/common/components/FeaturedApp.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import {motion} from 'framer-motion';
 import {cl} from '@builtbymom/web3/utils';
 
 import type {ReactElement} from 'react';
@@ -14,29 +13,26 @@ export function FeaturedApp(props: {app: TApp}): ReactElement {
 			className={cl(
 				'group relative flex cursor-pointer flex-col justify-end px-6 py-10 rounded-lg z-20 overflow-hidden outline outline-1 outline-gray-700/50 h-[272px] min-w-[272px]'
 			)}>
-			<motion.div
-				className={'absolute inset-0 bottom-[120px] md:bottom-0'}
-				initial={{y: 0}}
-				whileHover={{y: -50}}
-				transition={{duration: 0.3}}>
+			<div
+				className={
+					'absolute inset-0 bottom-[100px] transition-all duration-200 group-hover:bottom-[100px] md:bottom-0'
+				}>
 				<Image
 					src={props.app.logoURI}
 					alt={props.app.name}
 					priority={true}
 					width={2000}
 					height={2000}
-					className={'right-0 top-0 size-full bg-center object-cover transition-all duration-200'}
+					className={'right-0 top-0 size-full bg-center object-cover  '}
 				/>
-			</motion.div>
+			</div>
 
-			<motion.div
+			<div
 				className={
-					'pointer-events-none absolute bottom-[120px] left-0 z-20 h-[120px] w-full bg-gray-800 p-6 text-white transition-all md:bottom-0 md:group-hover:bottom-[120px]'
-				}
-				initial={{y: '100%'}}
-				transition={{duration: 0.2}}>
+					'pointer-events-none absolute bottom-0 left-0 z-30 h-[120px] w-full bg-gray-800 p-6 text-white transition-all md:bottom-[-120px] md:group-hover:bottom-0'
+				}>
 				{props.app.description}
-			</motion.div>
+			</div>
 		</Link>
 	);
 }
