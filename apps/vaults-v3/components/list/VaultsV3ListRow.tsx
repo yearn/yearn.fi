4cd628d0a77dc588992d49dc59105aaf85c6531b
Daniil Polienko <donyaschefer@gmail.com>
2024-11-21 16:07:28 +0300

    fix: low values

--- apps/vaults-v3/components/list/VaultsV3ListRow.tsx
+++ apps/vaults-v3/components/list/VaultsV3ListRow.tsx
@@ -578,7 +578,7 @@ export function VaultStakedAmount({currentVault}: {currentVault: TYDaemonVault})
 		currentVault.staking.available,
 		getToken
 	]);
-
+	console.log(staked);
 	return (
 		<div className={'flex flex-col pt-0 text-right md:pt-8'}>
 			<p
@@ -586,6 +586,7 @@ export function VaultStakedAmount({currentVault}: {currentVault: TYDaemonVault})
 					isZero(staked.raw) ? 'text-neutral-400' : 'text-neutral-900'
 				}`}>
 				<RenderAmount
+					shouldFormatDust
 					value={staked.normalized}
 					symbol={currentVault.token.symbol}
 					decimals={currentVault.token.decimals}
