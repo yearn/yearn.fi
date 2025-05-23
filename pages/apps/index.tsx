9e21f9fd18e292bd81f4778e27bfb743e9b21bae
rossgalloway <58150151+rossgalloway@users.noreply.github.com>
2025-05-23 10:04:06 -0400

    Chore: update apps page (#660)
    * update apps page to:
    * remove gimme from featured
    * remove juiced
    * add factory to vaults
    * remove yprisma from yearn apps
    * put juiced, yPrisma in "old apps"
    * remove search bar
    * add yprisma gauge to disabled list
    
    * chore: bump for vercel
    
    * fix: clean up
    
    * fix: remove comments
    
    * chore: add bearn and resupply and update wording
    
    * chore: update sidebar
    
    * chore: fix scroll in carousel
    
    * update header menus and mobile menus
    
    * update apps page
    
    * fix: bugs

--- pages/apps/index.tsx
+++ pages/apps/index.tsx
@@ -1,52 +1,54 @@
-import {type ReactElement, useRef} from 'react';
+import {type ReactElement} from 'react';
 import {useRouter} from 'next/router';
 import {useMountEffect} from '@react-hookz/web';
-import {CarouselSlideArrows} from '@common/CarouselSlideArrows';
-import {AppsCarousel} from '@common/components/AppsCarousel';
+// import {CarouselSlideArrows} from '@common/CarouselSlideArrows';
+// import {AppsCarousel} from '@common/components/AppsCarousel';
 import {CategorySection} from '@common/components/CategorySection';
 import {Cutaway} from '@common/components/Cutaway';
-import {PromoPoster} from '@common/components/PromoPoster';
+// import {PromoPoster} from '@common/components/PromoPoster';
 import {useSearch} from '@common/contexts/useSearch';
 import {LogoDiscord} from '@common/icons/LogoDiscord';
 import {LogoTwitter} from '@common/icons/LogoTwitter';
-import {FEATURED_APPS, INTEGRATIONS_APPS, VAULTS_APPS, YEARN_APPS, YEARN_X_APPS} from '@common/utils/constants';
+import {INTEGRATIONS_APPS, OLD_APPS, VAULTS_APPS, YEARN_APPS, YEARN_X_APPS} from '@common/utils/constants';
 
 export default function Home(): ReactElement {
 	const router = useRouter();
 	const {dispatch} = useSearch();
 
-	const carouselRef = useRef<HTMLDivElement | null>(null);
+	// const carouselRef = useRef<HTMLDivElement | null>(null);
 
-	const onScrollBack = (): void => {
-		if (!carouselRef.current) return;
-		carouselRef.current.scrollLeft -= 400;
-	};
+	// const onScrollBack = (): void => {
+	// 	if (!carouselRef.current) return;
+	// 	carouselRef.current.scrollLeft -= 400;
+	// };
 
-	const onScrollForward = (): void => {
-		if (!carouselRef.current) return;
-		carouselRef.current.scrollLeft += 400;
-	};
+	// const onScrollForward = (): void => {
+	// 	if (!carouselRef.current) return;
+	// 	carouselRef.current.scrollLeft += 400;
+	// };
 
 	useMountEffect(() => {
 		dispatch({searchValue: ''});
 	});
 
 	return (
-		<div className={'relative mb-4 mt-24 flex w-full justify-start md:mt-10'}>
+		<div className={'relative mb-4 mt-24 flex w-full justify-start bg-neutral-0 md:mt-10'}>
 			<div className={'w-full p-6 !pl-8 pb-24 pt-0 md:px-2'}>
 				<div className={'flex flex-col gap-y-14'}>
-					<div className={'md:hidden'}>
+					{/* <div className={'md:hidden'}>
 						<PromoPoster />
-					</div>
+					</div> */}
 
-					<div>
+					{/* <div>
 						<div className={'mb-6 flex items-start justify-between'}>
-							<p className={'w-full text-lg font-bold text-white'}>{'Featured Apps'}</p>
-							<CarouselSlideArrows
-								onScrollBack={onScrollBack}
-								onScrollForward={onScrollForward}
-								className={'w-auto'}
-							/>
+							<p className={'w-full text-lg font-bold text-white'}>{'Featured Products'}</p>
+							{FEATURED_APPS?.length > 3 && (
+								<CarouselSlideArrows
+									onScrollBack={onScrollBack}
+									onScrollForward={onScrollForward}
+									className={'w-auto'}
+								/>
+							)}
 						</div>
 
 						<AppsCarousel
@@ -54,16 +56,16 @@ export default function Home(): ReactElement {
 							apps={FEATURED_APPS}
 							isUsingFeatured={true}
 						/>
-					</div>
+					</div> */}
 
 					<div className={'flex flex-col gap-10'}>
 						<CategorySection
-							title={'Vaults'}
+							title={'Yearn Vaults'}
 							onExpandClick={async () => router.push('/apps/vaults')}
 							apps={VAULTS_APPS}
 						/>
 						<CategorySection
-							title={'Yearn Apps'}
+							title={'Other Yearn Products'}
 							onExpandClick={async () => router.push('/apps/yearn-apps')}
 							apps={YEARN_APPS}
 						/>
@@ -77,17 +79,22 @@ export default function Home(): ReactElement {
 							onExpandClick={async () => router.push('/apps/integrations')}
 							apps={INTEGRATIONS_APPS}
 						/>
+						<CategorySection
+							title={'Retired Apps'}
+							onExpandClick={async () => router.push('/apps/retired-apps')}
+							apps={OLD_APPS}
+						/>
 					</div>
 				</div>
 				<div className={'mt-16 flex w-full flex-col gap-6 md:flex-row'}>
 					<Cutaway
 						title={'Follow us on X'}
-						icon={<LogoTwitter className={'text-white'} />}
+						icon={<LogoTwitter className={'text-neutral-800'} />}
 						link={'https://yearn.finance/twitter'}
 					/>
 					<Cutaway
 						title={'Join our Discord'}
-						icon={<LogoDiscord className={'text-white'} />}
+						icon={<LogoDiscord className={'text-neutral-800'} />}
 						link={'https://discord.com/invite/yearn'}
 					/>
 				</div>
