7585669973dadedbfc112e21eff682f51eef1603
0xeye <97349378+0xeye@users.noreply.github.com>
2025-05-23 17:04:15 +0100

    feat: add live vaults data

--- apps/landing/components/sections/Vaults.tsx
+++ apps/landing/components/sections/Vaults.tsx
@@ -1,13 +1,16 @@
-import {FC, useState, useEffect, useCallback} from 'react';
+import {FC, useState, useEffect, useCallback, useMemo} from 'react';
 import Image from 'next/image';
 import {SectionHeader} from 'apps/landing/components/common/SectionHeader';
+import {useYearn} from '@common/contexts/useYearn';
+import {formatPercent} from '@builtbymom/web3/utils/format';
 
-interface VaultRow {
+interface Row {
 	bgClass: string;
 	icon: string;
 	text: string;
 	description?: string;
 	href: string;
+	address?: string;
 }
 
 interface Vault {
@@ -18,7 +21,6 @@ interface Vault {
 	title: string;
 	description: string;
 	cta?: {label: string; href: string};
-	vaultRows: VaultRow[];
 }
 
 const slides: Vault[] = [
@@ -30,27 +32,7 @@ const slides: Vault[] = [
 		title: 'Compounding Vaults',
 		description:
 			'Yearn Vaults take advantage of DeFi opportunities to give you the best risk-adjusted yields without you having to lift a finger',
-		cta: {label: 'View All', href: 'https://yearn.fi/apps/vaults'},
-		vaultRows: [
-			{
-				bgClass: 'bg-gradient-to-r from-gray-800 to-gray-700',
-				icon: '/landing/vaults/eth.png',
-				text: 'Earn 4.75% on ETH',
-				href: '/v3/1/0xc56413869c6CDf96496f2b1eF801fEDBdFA7dDB0'
-			},
-			{
-				bgClass: 'bg-gradient-to-r from-gray-900 to-gray-800',
-				icon: '/landing/vaults/usds.png',
-				text: 'Earn 5.25% on USDS',
-				href: '/v3/1/0x182863131F9a4630fF9E27830d945B1413e347E8'
-			},
-			{
-				bgClass: 'bg-gradient-to-r from-gray-900 to-gray-700',
-				icon: '/landing/vaults/usdc.png',
-				text: 'Earn 3.95% on USDC',
-				href: '/v3/1/0xBe53A109B494E5c9f97b9Cd39Fe969BE68BF6204'
-			}
-		]
+		cta: {label: 'View All', href: 'https://yearn.fi/apps/vaults'}
 	},
 	{
 		background: '/landing/vault-background-y.png',
@@ -58,43 +40,90 @@ const slides: Vault[] = [
 		size: 300,
 		tagline: 'Growing every day',
 		title: 'App Ecosystem',
-		description: 'Apps built on Yearn vaults by contributors and the wider community',
-		vaultRows: [
-			{
-				bgClass: 'bg-gradient-to-r from-gray-800 to-gray-700',
-				icon: '/landing/apps/veyfi.png',
-				text: 'veYFI',
-				description: 'Earn yield, boost gauges, and take part in governance',
-				href: 'https://veyfi.yearn.fi/'
-			},
-			{
-				bgClass: 'bg-gradient-to-r from-gray-900 to-gray-800',
-				icon: '/landing/apps/ycrv.png',
-				text: 'yCRV',
-				description: 'Put your yCRV to work',
-				href: 'https://ycrv.yearn.fi/'
-			},
-			{
-				bgClass: 'bg-gradient-to-r from-gray-800 to-gray-700',
-				icon: '/landing/apps/yprisma.png',
-				text: 'yPRISMA',
-				description: 'Put your yPRISMA to work',
-				href: 'https://yprisma.yearn.fi/'
-			},
-			{
-				bgClass: 'bg-gradient-to-r from-gray-900 to-gray-800',
-				icon: '/landing/apps/yeth.png',
-				text: 'yETH',
-				description: 'A basket of LSTs in a single token',
-				href: 'https://yeth.yearn.fi/'
-			}
-		]
+		description: 'Apps built on Yearn vaults by contributors and the wider community'
+	}
+];
+
+const vaultsRows = [
+	{
+		bgClass: 'bg-gradient-to-r from-gray-800 to-gray-700',
+		symbol: 'ETH',
+		icon: '/landing/vaults/eth.png',
+		href: '/v3/1/0xc56413869c6CDf96496f2b1eF801fEDBdFA7dDB0',
+		address: '0xc56413869c6CDf96496f2b1eF801fEDBdFA7dDB0'
+	},
+	{
+		bgClass: 'bg-gradient-to-r from-gray-900 to-gray-800',
+		symbol: 'USDS',
+		icon: '/landing/vaults/usds.png',
+		href: '/v3/1/0x182863131F9a4630fF9E27830d945B1413e347E8',
+		address: '0x182863131F9a4630fF9E27830d945B1413e347E8'
+	},
+	{
+		bgClass: 'bg-gradient-to-r from-gray-900 to-gray-700',
+		symbol: 'USDC',
+		icon: '/landing/vaults/usdc.png',
+		href: '/v3/1/0xBe53A109B494E5c9f97b9Cd39Fe969BE68BF6204',
+		address: '0xBe53A109B494E5c9f97b9Cd39Fe969BE68BF6204'
+	}
+];
+
+const appRows: Row[] = [
+	{
+		bgClass: 'bg-gradient-to-r from-gray-800 to-gray-700',
+		icon: '/landing/apps/veyfi.png',
+		text: 'veYFI',
+		description: 'Earn yield, boost gauges, and take part in governance',
+		href: 'https://veyfi.yearn.fi/'
+	},
+	{
+		bgClass: 'bg-gradient-to-r from-gray-900 to-gray-800',
+		icon: '/landing/apps/ycrv.png',
+		text: 'yCRV',
+		description: 'Put your yCRV to work',
+		href: 'https://ycrv.yearn.fi/'
+	},
+	{
+		bgClass: 'bg-gradient-to-r from-gray-800 to-gray-700',
+		icon: '/landing/apps/yprisma.png',
+		text: 'yPRISMA',
+		description: 'Put your yPRISMA to work',
+		href: 'https://yprisma.yearn.fi/'
+	},
+	{
+		bgClass: 'bg-gradient-to-r from-gray-900 to-gray-800',
+		icon: '/landing/apps/yeth.png',
+		text: 'yETH',
+		description: 'A basket of LSTs in a single token',
+		href: 'https://yeth.yearn.fi/'
 	}
 ];
 
 export const Vaults: FC = () => {
+	const {vaults, isLoadingVaultList} = useYearn();
 	const [activeSlide, setActiveSlide] = useState(0);
 	const [isAnimating, setIsAnimating] = useState(false);
+
+	const rows: Row[][] = useMemo(() => {
+		return [
+			vaultsRows.map((vault, index) => {
+				const apr = vaults[vault.address]?.apr.forwardAPR.netAPR;
+				return {
+					bgClass:
+						index % 2 === 0
+							? 'bg-gradient-to-r from-gray-800 to-gray-700'
+							: 'bg-gradient-to-r from-gray-900 to-gray-800',
+					icon: vaultsRows[index].icon,
+					text: apr
+						? `Earn ${formatPercent(apr * 100, 2, 2)} on ${vaultsRows[index].symbol}`
+						: `Earn on ${vaultsRows[index].symbol}`,
+					href: `/v3/1/${vault.address}`,
+					address: vault.address
+				};
+			}) as Row[],
+			appRows
+		];
+	}, [vaults, vaultsRows]);
 
 	const totalSlides = slides.length;
 
@@ -137,7 +166,6 @@ export const Vaults: FC = () => {
 							<div
 								key={index}
 								className="w-full flex-shrink-0 flex flex-col md:flex-row items-stretch justify-between gap-6 md:gap-8">
-								{/* Image Container */}
 								<div className="flex w-full md:w-2/5 relative min-h-[300px] md:min-h-auto">
 									<div
 										className="flex h-full w-full border-[1px] border-[#ffffff]/10 rounded-[24px] items-center justify-center relative overflow-hidden"
@@ -171,48 +199,60 @@ export const Vaults: FC = () => {
 
 									<div className="relative p-1 bg-white/5 rounded-[24px] overflow-hidden p-[8px] w-full">
 										<div className="space-y-1">
-											{slide.vaultRows.map((vault, vaultIndex) => (
-												<a
-													key={vaultIndex}
-													href={vault.href}
-													className={`${vault.bgClass} rounded-[16px] flex items-center justify-between p-2 md:p-[8px] cursor-pointer hover:opacity-90 transition-opacity duration-200`}>
-													<div className="flex items-center">
-														<div className=" rounded-2xl p-1 mr-2 md:mr-3">
-															<div className="w-5 h-5 md:w-6 md:h-6 bg-gray-900 rounded-2xl flex items-center justify-center">
-																<Image
-																	src={vault.icon}
-																	alt={vault.text}
-																	width={24}
-																	height={24}
-																/>
+											{rows[activeSlide].map((row, vaultIndex) => {
+												const isVaultLoading = row?.address && isLoadingVaultList;
+
+												return (
+													<a
+														key={vaultIndex}
+														href={row.href}
+														className={`${row.bgClass} rounded-[16px] flex items-center justify-between p-2 md:p-[8px] cursor-pointer hover:opacity-50 transition-opacity duration-200`}>
+														<div className="flex items-center">
+															<div className=" rounded-2xl p-1 mr-2 md:mr-3">
+																<div className="w-5 h-5 md:w-6 md:h-6 bg-gray-900 rounded-2xl flex items-center justify-center">
+																	<Image
+																		src={row.icon}
+																		alt={row.text}
+																		width={24}
+																		height={24}
+																	/>
+																</div>
+															</div>
+															<div className="flex flex-col md:flex-row gap-1 md:gap-2">
+																{isVaultLoading ? (
+																	<div className="flex items-center space-x-2">
+																		<div className="animate-pulse bg-gray-600 h-4 w-24 rounded"></div>
+																	</div>
+																) : (
+																	<span className=":text-base font-medium">
+																		{row.text}
+																	</span>
+																)}
+																{row.description && (
+																	<span className="md:text-base font-light text-gray-400">
+																		{row.description}
+																	</span>
+																)}
 															</div>
 														</div>
-														<div className="flex flex-col md:flex-row gap-1 md:gap-2">
-															<span className=":text-base font-medium">{vault.text}</span>
-															{vault.description && (
-																<span className="md:text-base font-light text-gray-400">
-																	{vault.description}
-																</span>
-															)}
+														<div className="bg-transparent p-1 md:p-2 rounded-2xl">
+															<svg
+																xmlns="http://www.w3.org/2000/svg"
+																className="h-5 w-5 md:h-6 md:w-6"
+																fill="none"
+																viewBox="0 0 24 24"
+																stroke="currentColor">
+																<path
+																	strokeLinecap="round"
+																	strokeLinejoin="round"
+																	strokeWidth={2}
+																	d="M14 5l7 7m0 0l-7 7m7-7H3"
+																/>
+															</svg>
 														</div>
-													</div>
-													<div className="bg-transparent p-1 md:p-2 rounded-2xl">
-														<svg
-															xmlns="http://www.w3.org/2000/svg"
-															className="h-5 w-5 md:h-6 md:w-6"
-															fill="none"
-															viewBox="0 0 24 24"
-															stroke="currentColor">
-															<path
-																strokeLinecap="round"
-																strokeLinejoin="round"
-																strokeWidth={2}
-																d="M14 5l7 7m0 0l-7 7m7-7H3"
-															/>
-														</svg>
-													</div>
-												</a>
-											))}
+													</a>
+												);
+											})}
 										</div>
 									</div>
 								</div>
