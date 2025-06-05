c7406813902a11939ba73d2babc581718ce196b0
rossgalloway <58150151+rossgalloway@users.noreply.github.com>
2025-06-05 11:42:24 -0400

    Chore: update /apps page (#675)
    * update apps page to:
    * remove gimme from featured
    * remove juiced
    * add factory to vaults
    * remove yprisma from yearn apps
    * put juiced, yPrisma in "old apps"
    * remove search bar
    * add yprisma gauge to disabled list
    
    * chore: bump for vercel
    
    * fix: clean up
    
    * fix: remove comments
    
    * chore: add bearn and resupply and update wording
    
    * chore: update sidebar
    
    * chore: fix scroll in carousel
    
    * update header menus and mobile menus
    
    * update apps page
    
    * fix: bugs
    
    * fix: card styling
    
    * chore: fix mobile layout and resupply logo
    
    * remove category expand, clean up shuffle
    
    * chore: clean up
    
    * chore: remove page animations
    
    * chore: add back subtle fade in and out for page transitions

--- apps/lib/icons/IconShare.tsx
+++ apps/lib/icons/IconShare.tsx
@@ -1,6 +1,6 @@
 import type {ReactElement} from 'react';
 
-export function IconShare(props: React.SVGProps<SVGSVGElement>): ReactElement {
+export function IconShare({stroke = 'currentColor', ...props}: React.SVGProps<SVGSVGElement>): ReactElement {
 	return (
 		<svg
 			{...props}
@@ -11,14 +11,14 @@ export function IconShare(props: React.SVGProps<SVGSVGElement>): ReactElement {
 			xmlns={'http://www.w3.org/2000/svg'}>
 			<path
 				d={'M5 1H11V7'}
-				stroke={'white'}
+				stroke={stroke}
 				strokeWidth={'2'}
 				strokeLinecap={'round'}
 				strokeLinejoin={'round'}
 			/>
 			<path
 				d={'M11 1L1 11'}
-				stroke={'white'}
+				stroke={stroke}
 				strokeWidth={'2'}
 				strokeLinecap={'round'}
 				strokeLinejoin={'round'}
