21bdb512c593ae3568683bd114de853f4fedd959
0xeye <97349378+0xeye@users.noreply.github.com>
2025-09-08 16:48:09 +0100

    chore: remove nextgen

--- next.config.js
+++ next.config.js
@@ -35,8 +35,7 @@ module.exports = withPlausibleProxy({
       ...config.resolve,
       alias: {
         ...config.resolve.alias,
-        '@safe-global/safe-apps-sdk': path.resolve(__dirname, '../../node_modules/@safe-global/safe-apps-sdk/dist/esm'),
-        '@nextgen/components': path.resolve(__dirname, '../nextgen/src/components/elements/index.ts')
+        '@safe-global/safe-apps-sdk': path.resolve(__dirname, 'node_modules/@safe-global/safe-apps-sdk/dist/esm')
       },
       fallback: {
         ...config.resolve.fallback,
