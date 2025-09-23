43859852d197e37565f1205ac26528ca1ab64282
Daniil Polienko <donyaschefer@gmail.com>
2025-09-23 18:49:59 +0300

    chore: add logs

--- apps/lib/hooks/useBalances.multichains.ts
+++ apps/lib/hooks/useBalances.multichains.ts
@@ -98,6 +98,7 @@ export async function performCall(
       }
   )[] = []
   try {
+    console.log('performCall', chunckCalls)
     results = await multicall(retrieveConfig(), {
       contracts: chunckCalls as never[],
       chainId: chainID
@@ -271,6 +272,7 @@ export async function getBalances(
 
   try {
     const [callResult] = await performCall(chainID, calls, tokens, toAddress(ownerAddress))
+    console.log('callResult', tokens, callResult)
     result = { ...result, ...callResult }
     return [result, undefined]
   } catch (_error) {
@@ -396,6 +398,7 @@ export function useBalances(props?: TUseBalancesReq): TUseBalancesRes {
       }
 
       const allPromises = chunks.map(async (chunkTokens) => {
+        console.log('chunkTokens', chunkTokens)
         const [newRawData, err] = await getBalances(chainID, userAddress, chunkTokens, shouldForceFetch)
         if (err) {
           options.setError(err)
