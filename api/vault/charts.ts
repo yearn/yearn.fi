650908a453bc1f0373c9e6f83299256aa39a4911
Ross <therossgalloway@gmail.com>
2025-11-19 14:15:16 -0500

    fix: CORS issues

--- api/vault/charts.ts
+++ api/vault/charts.ts
@@ -67,6 +67,14 @@ const VAULT_CHARTS_QUERY = `
 `
 
 export default async function handler(request: VercelRequest, response: VercelResponse) {
+  response.setHeader('Access-Control-Allow-Origin', '*')
+  response.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS')
+  response.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+
+  if (request.method === 'OPTIONS') {
+    return response.status(204).end()
+  }
+
   if (request.method !== 'GET') {
     response.setHeader('Allow', 'GET')
     return response.status(405).json({ error: 'Method not allowed' })
