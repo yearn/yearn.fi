4f8d77dd5db5a99ddd8d6a5e8932c72c8bfc5813
Ross <ross@yearn.finance>
2025-12-02 11:45:02 -0500

    feat: add outline logo

--- apps/lib/icons/LogoYearnMarkOutline.tsx
+++ apps/lib/icons/LogoYearnMarkOutline.tsx
@@ -11,29 +11,26 @@ export function LogoYearnMarkOutline(props: React.SVGProps<SVGSVGElement> & { co
       role={'img'}
       {...svgProps}
     >
-      <defs>
-        <clipPath id={'logo-mark-outline-clip'}>
-          <g transform={'translate(6 6) scale(0.95)'}>
-            <path
-              d={
-                'M201.9,98.5l-32.4,32.4c1.8,5.9,2.7,12.2,2.7,18.5c0,17.1-6.7,33.2-18.7,45.3c-12.1,12.1-28.2,18.7-45.3,18.7s-33.2-6.7-45.3-18.7c-12.1-12.1-18.7-28.2-18.7-45.3c0-6.4,0.9-12.6,2.7-18.5L14.4,98.5c-8.2,15.1-12.9,32.5-12.9,51c0,58.9,47.8,106.7,106.7,106.7s106.7-47.8,106.7-106.7C214.9,131,210.2,113.6,201.9,98.5z'
-              }
-            />
-            <polygon
-              points={'86.8,170.7 129.5,170.7 129.5,102.9 202.2,30.2 172,0 108.2,63.8 44.4,0 14.2,30.2 86.8,102.8'}
-            />
-          </g>
-        </clipPath>
-      </defs>
-
-      <g clipPath={'url(#logo-mark-outline-clip)'} transform={'translate(6 6) scale(0.95)'}>
+      <g transform={'translate(6 6) scale(0.95)'}>
+        <defs>
+          <filter
+            id={'logo-mark-outline-shadow'}
+            x={'-30%'}
+            y={'-30%'}
+            width={'160%'}
+            height={'160%'}
+            colorInterpolationFilters={'sRGB'}
+          >
+            <feDropShadow dx={'0'} dy={'2'} stdDeviation={'3'} floodColor={color} floodOpacity={'0.25'} />
+          </filter>
+        </defs>
         <path
           fill={'none'}
           stroke={color}
-          strokeWidth={12}
+          strokeWidth={8}
           strokeLinejoin={'round'}
           strokeLinecap={'round'}
-          vectorEffect={'non-scaling-stroke'}
+          filter={'url(#logo-mark-outline-shadow)'}
           d={
             'M201.9,98.5l-32.4,32.4c1.8,5.9,2.7,12.2,2.7,18.5c0,17.1-6.7,33.2-18.7,45.3c-12.1,12.1-28.2,18.7-45.3,18.7s-33.2-6.7-45.3-18.7c-12.1-12.1-18.7-28.2-18.7-45.3c0-6.4,0.9-12.6,2.7-18.5L14.4,98.5c-8.2,15.1-12.9,32.5-12.9,51c0,58.9,47.8,106.7,106.7,106.7s106.7-47.8,106.7-106.7C214.9,131,210.2,113.6,201.9,98.5z'
           }
@@ -41,10 +38,10 @@ export function LogoYearnMarkOutline(props: React.SVGProps<SVGSVGElement> & { co
         <polygon
           fill={'none'}
           stroke={color}
-          strokeWidth={12}
+          strokeWidth={8}
           strokeLinejoin={'round'}
           strokeLinecap={'round'}
-          vectorEffect={'non-scaling-stroke'}
+          filter={'url(#logo-mark-outline-shadow)'}
           points={'86.8,170.7 129.5,170.7 129.5,102.9 202.2,30.2 172,0 108.2,63.8 44.4,0 14.2,30.2 86.8,102.8'}
         />
       </g>
