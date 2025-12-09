40437034ed82f9dce571c3c993850333409a41ad
w84april <donyaschefer@gmail.com>
2025-12-09 20:56:08 +0300

    feat: swap places

--- apps/vaults-v3/components/TrendingVaults.tsx
+++ apps/vaults-v3/components/TrendingVaults.tsx
@@ -71,43 +71,45 @@ export function TrendingVaults({ suggestedVaults }: TTrendingVaultsProps): React
     <div className={'w-full bg-app pb-2'}>
       <div className={'flex flex-col gap-0 rounded-xl border border-neutral-200 bg-surface'}>
         <div className={'flex w-full items-center justify-between gap-3 px-4 py-3 md:px-6 md:py-4'}>
+          <div className={'flex items-center gap-3'}>
+            <div className={'flex flex-col text-left'}>
+              <p className={'text-sm font-semibold tracking-wide text-neutral-500'}>{'Trending Vaults'}</p>
+            </div>
+            {isTrendingExpanded && suggestedVaults.length > 4 ? (
+              <div className={'hidden gap-3 md:flex'}>
+                <button
+                  onClick={onScrollBack}
+                  disabled={!canScrollLeft}
+                  className={cl(
+                    'flex h-5! items-center rounded-[4px] px-2 outline-solid outline-1! outline-neutral-200 transition-colors ',
+                    canScrollLeft ? 'text-neutral-400 hover:bg-neutral-200' : 'text-neutral-300 cursor-not-allowed'
+                  )}
+                >
+                  <IconChevron className={'size-3 rotate-90'} />
+                </button>
+                <button
+                  onClick={onScrollForward}
+                  disabled={!canScrollRight}
+                  className={cl(
+                    'flex h-5! items-center rounded-[4px] px-2  transition-colors outline-solid outline-1! outline-neutral-200',
+                    canScrollRight ? 'text-neutral-400 hover:bg-neutral-200' : 'text-neutral-300 cursor-not-allowed'
+                  )}
+                >
+                  <IconChevron className={'size-3 -rotate-90'} />
+                </button>
+              </div>
+            ) : null}
+          </div>
           <button
             type={'button'}
             className={'flex items-center gap-3'}
             onClick={(): void => setIsTrendingExpanded((previous) => !previous)}
           >
-            <div className={'flex flex-col text-left'}>
-              <p className={'text-sm font-semibold tracking-wide text-neutral-500'}>{'Trending Vaults'}</p>
-            </div>
             <IconChevron
               className={'size-4 text-neutral-600 transition-transform duration-200'}
               direction={isTrendingExpanded ? 'up' : 'down'}
             />
           </button>
-          {isTrendingExpanded && suggestedVaults.length > 4 ? (
-            <div className={'hidden gap-3 md:flex'}>
-              <button
-                onClick={onScrollBack}
-                disabled={!canScrollLeft}
-                className={cl(
-                  'flex h-5! items-center rounded-[4px] px-2 outline-solid outline-1! outline-neutral-200 transition-colors ',
-                  canScrollLeft ? 'text-neutral-400 hover:bg-neutral-200' : 'text-neutral-300 cursor-not-allowed'
-                )}
-              >
-                <IconChevron className={'size-3 rotate-90'} />
-              </button>
-              <button
-                onClick={onScrollForward}
-                disabled={!canScrollRight}
-                className={cl(
-                  'flex h-5! items-center rounded-[4px] px-2  transition-colors outline-solid outline-1! outline-neutral-200',
-                  canScrollRight ? 'text-neutral-400 hover:bg-neutral-200' : 'text-neutral-300 cursor-not-allowed'
-                )}
-              >
-                <IconChevron className={'size-3 -rotate-90'} />
-              </button>
-            </div>
-          ) : null}
         </div>
         {isTrendingExpanded ? (
           <div
