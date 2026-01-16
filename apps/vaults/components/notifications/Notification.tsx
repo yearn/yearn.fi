97515f46ffff7f894ba7300af4151b90c2183b6b
w84april <donyaschefer@gmail.com>
2026-01-16 21:55:39 +0300

    chore: secondary restore

--- apps/vaults/components/notifications/Notification.tsx
+++ apps/vaults/components/notifications/Notification.tsx
@@ -10,7 +10,6 @@ import type { TNotification, TNotificationStatus } from '@lib/types/notification
 import { cl, SUPPORTED_NETWORKS, truncateHex } from '@lib/utils'
 import type { ReactElement } from 'react'
 import { memo, useCallback, useMemo, useState } from 'react'
-import Image from '/src/components/Image'
 import Link from '/src/components/Link'
 
 const NETWORK_BY_CHAIN_ID = new Map(SUPPORTED_NETWORKS.map((network) => [network.id, network] as const)) as ReadonlyMap<
@@ -55,26 +54,16 @@ function ApproveNotificationContent({ notification }: { notification: TNotificat
   return (
     <div className={'flex gap-4'}>
       <div className={'flex flex-col items-center gap-3'}>
-        <div className={'relative'}>
-          <TokenLogo
-            src={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/tokens/${notification.chainId}/${notification.fromAddress ? notification.fromAddress.toLowerCase() : '0x0'}/logo-32.png`}
-            altSrc={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/tokens/${notification.chainId}/${notification.fromAddress ? notification.fromAddress.toLowerCase() : '0x0'}/logo-32.png`}
-            tokenSymbol={notification.fromTokenName}
-            width={32}
-            height={32}
-            className="rounded-full"
-            loading="eager"
-          />
-          <div className={'absolute bottom-6 left-5 flex size-4 items-center justify-center rounded-full bg-white'}>
-            <Image
-              className={'object-contain'}
-              width={14}
-              height={14}
-              alt={'chain'}
-              src={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/chains/${notification.chainId}/logo.svg`}
-            />
-          </div>
-        </div>
+        <TokenLogo
+          src={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/tokens/${notification.chainId}/${notification.fromAddress ? notification.fromAddress.toLowerCase() : '0x0'}/logo-32.png`}
+          altSrc={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/tokens/${notification.chainId}/${notification.fromAddress ? notification.fromAddress.toLowerCase() : '0x0'}/logo-32.png`}
+          tokenSymbol={notification.fromTokenName}
+          chainId={notification.chainId}
+          width={32}
+          height={32}
+          className="rounded-full"
+          loading="eager"
+        />
       </div>
       <div className={'flex-1'}>
         <div className={'grid grid-cols-2 gap-x-4 gap-y-2 text-xs text-text-primary'}>
@@ -151,51 +140,30 @@ function DepositNotificationContent({ notification }: { notification: TNotificat
   return (
     <div className={'flex gap-4'}>
       <div className={'flex flex-col items-center gap-3'}>
-        <div className={'relative'}>
+        <TokenLogo
+          src={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/tokens/${notification.chainId}/${notification.fromAddress ? notification.fromAddress.toLowerCase() : '0x0'}/logo-32.png`}
+          altSrc={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/tokens/${notification.chainId}/${notification.fromAddress ? notification.fromAddress.toLowerCase() : '0x0'}/logo-32.png`}
+          tokenSymbol={notification.fromTokenName}
+          chainId={notification.chainId}
+          width={32}
+          height={32}
+          className="rounded-full"
+          loading="eager"
+        />
+
+        {notification.toTokenName && <IconArrow className={'size-4 rotate-135'} />}
+
+        {notification.toTokenName && notification.toAddress && (
           <TokenLogo
-            src={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/tokens/${notification.chainId}/${notification.fromAddress ? notification.fromAddress.toLowerCase() : '0x0'}/logo-32.png`}
-            altSrc={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/tokens/${notification.chainId}/${notification.fromAddress ? notification.fromAddress.toLowerCase() : '0x0'}/logo-32.png`}
-            tokenSymbol={notification.fromTokenName}
+            src={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/tokens/${notification.toChainId || notification.chainId}/${notification.toAddress.toLowerCase()}/logo-32.png`}
+            altSrc={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/tokens/${notification.toChainId || notification.chainId}/${notification.toAddress.toLowerCase()}/logo-32.png`}
+            tokenSymbol={notification.toTokenName}
+            chainId={notification.toChainId || notification.chainId}
             width={32}
             height={32}
             className="rounded-full"
             loading="eager"
           />
-          <div className={'absolute bottom-6 left-5 flex size-4 items-center justify-center rounded-full bg-white'}>
-            <Image
-              className={'object-contain'}
-              width={14}
-              height={14}
-              alt={'chain'}
-              src={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/chains/${notification.chainId}/logo.svg`}
-            />
-          </div>
-        </div>
-
-        {notification.toTokenName && notification.toAddress && (
-          <>
-            <IconArrow className={'size-4 rotate-135'} />
-            <div className={'relative'}>
-              <TokenLogo
-                src={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/tokens/${notification.toChainId || notification.chainId}/${notification.toAddress.toLowerCase()}/logo-32.png`}
-                altSrc={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/tokens/${notification.toChainId || notification.chainId}/${notification.toAddress.toLowerCase()}/logo-32.png`}
-                tokenSymbol={notification.toTokenName}
-                width={32}
-                height={32}
-                className="rounded-full"
-                loading="eager"
-              />
-              <div className={'absolute bottom-6 left-5 flex size-4 items-center justify-center rounded-full bg-white'}>
-                <Image
-                  className={'object-contain'}
-                  width={14}
-                  height={14}
-                  alt={'chain'}
-                  src={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/chains/${notification.toChainId || notification.chainId}/logo.svg`}
-                />
-              </div>
-            </div>
-          </>
         )}
       </div>
       <div className={'flex-1'}>
@@ -282,51 +250,30 @@ function WithdrawNotificationContent({ notification }: { notification: TNotifica
   return (
     <div className={'flex gap-4'}>
       <div className={'flex flex-col items-center gap-3'}>
-        <div className={'relative'}>
+        <TokenLogo
+          src={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/tokens/${notification.chainId}/${notification.fromAddress ? notification.fromAddress.toLowerCase() : '0x0'}/logo-32.png`}
+          altSrc={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/tokens/${notification.chainId}/${notification.fromAddress ? notification.fromAddress.toLowerCase() : '0x0'}/logo-32.png`}
+          tokenSymbol={notification.fromTokenName}
+          chainId={notification.chainId}
+          width={32}
+          height={32}
+          className="rounded-full"
+          loading="eager"
+        />
+
+        {notification.toTokenName && <IconArrow className={'size-4 rotate-135'} />}
+
+        {notification.toTokenName && notification.toAddress && (
           <TokenLogo
-            src={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/tokens/${notification.chainId}/${notification.fromAddress ? notification.fromAddress.toLowerCase() : '0x0'}/logo-32.png`}
-            altSrc={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/tokens/${notification.chainId}/${notification.fromAddress ? notification.fromAddress.toLowerCase() : '0x0'}/logo-32.png`}
-            tokenSymbol={notification.fromTokenName}
+            src={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/tokens/${notification.toChainId || notification.chainId}/${notification.toAddress.toLowerCase()}/logo-32.png`}
+            altSrc={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/tokens/${notification.toChainId || notification.chainId}/${notification.toAddress.toLowerCase()}/logo-32.png`}
+            tokenSymbol={notification.toTokenName}
+            chainId={notification.toChainId || notification.chainId}
             width={32}
             height={32}
             className="rounded-full"
             loading="eager"
           />
-          <div className={'absolute bottom-6 left-5 flex size-4 items-center justify-center rounded-full bg-white'}>
-            <Image
-              className={'object-contain'}
-              width={14}
-              height={14}
-              alt={'chain'}
-              src={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/chains/${notification.chainId}/logo.svg`}
-            />
-          </div>
-        </div>
-
-        {notification.toTokenName && notification.toAddress && (
-          <>
-            <IconArrow className={'size-4 rotate-135'} />
-            <div className={'relative'}>
-              <TokenLogo
-                src={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/tokens/${notification.toChainId || notification.chainId}/${notification.toAddress.toLowerCase()}/logo-32.png`}
-                altSrc={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/tokens/${notification.toChainId || notification.chainId}/${notification.toAddress.toLowerCase()}/logo-32.png`}
-                tokenSymbol={notification.toTokenName}
-                width={32}
-                height={32}
-                className="rounded-full"
-                loading="eager"
-              />
-              <div className={'absolute bottom-6 left-5 flex size-4 items-center justify-center rounded-full bg-white'}>
-                <Image
-                  className={'object-contain'}
-                  width={14}
-                  height={14}
-                  alt={'chain'}
-                  src={`${import.meta.env.VITE_BASE_YEARN_ASSETS_URI}/chains/${notification.toChainId || notification.chainId}/logo.svg`}
-                />
-              </div>
-            </div>
-          </>
         )}
       </div>
       <div className={'flex-1'}>
