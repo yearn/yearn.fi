c15bfb43de7fcd4c00573d4d7ca1f13fedd7305f
w84april <donyaschefer@gmail.com>
2026-03-05 19:31:50 +0300

    chore: rm dead code

--- src/components/pages/portfolio/types/api.ts
+++ src/components/pages/portfolio/types/api.ts
@@ -1,31 +1,6 @@
 import { z } from 'zod'
 
-export const portfolioHistoryVaultSchema = z.object({
-  address: z.string(),
-  shares: z.string(),
-  usdValue: z.number()
-})
-
-export const portfolioHistoryChainSchema = z.object({
-  chainId: z.number(),
-  chainName: z.string(),
-  totalUsdValue: z.number(),
-  vaults: z.array(portfolioHistoryVaultSchema)
-})
-
-export const portfolioHistoryDataPointSchema = z.object({
-  date: z.string(),
-  totalUsdValue: z.number(),
-  chains: z.array(portfolioHistoryChainSchema)
-})
-
-export const portfolioHistoryResponseSchema = z.object({
-  address: z.string(),
-  periodDays: z.number(),
-  dataPoints: z.array(portfolioHistoryDataPointSchema)
-})
-
-export const portfolioHistorySimpleDataPointSchema = z.object({
+const portfolioHistorySimpleDataPointSchema = z.object({
   date: z.string(),
   value: z.number()
 })
@@ -35,9 +10,7 @@ export const portfolioHistorySimpleResponseSchema = z.object({
   dataPoints: z.array(portfolioHistorySimpleDataPointSchema)
 })
 
-export type TPortfolioHistoryResponse = z.infer<typeof portfolioHistoryResponseSchema>
 export type TPortfolioHistorySimpleResponse = z.infer<typeof portfolioHistorySimpleResponseSchema>
-export type TPortfolioHistoryDataPoint = z.infer<typeof portfolioHistoryDataPointSchema>
 export type TPortfolioHistoryChartData = Array<{
   date: string
   totalUsdValue: number
