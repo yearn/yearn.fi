0a75b35068ea88daec7bbcd81240f191ef974a3b
rossgalloway <58150151+rossgalloway@users.noreply.github.com>
2026-05-08 20:27:07 +0000

    lint

--- src/components/pages/vaults/components/widget/shared/transactionOverlay.helpers.test.ts
+++ src/components/pages/vaults/components/widget/shared/transactionOverlay.helpers.test.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from 'vitest'
-import { getInitialOverlayState } from './transactionOverlay.helpers'
 import {
   formatPendingTransactionFunctionName,
+  getInitialOverlayState,
   getPendingTransactionTitle,
   resolveCompletionDeferral,
   shouldAutoContinuePermitSuccess,
